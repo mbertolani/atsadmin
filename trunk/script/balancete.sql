5b4242ba4e3aaf55883aeff41ff19bcda8ada400
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2009-12-11 19:57:20 +0000

    correcoes.

--- trunk/script/balancete.sql
+++ trunk/script/balancete.sql
@@ -8,16 +8,20 @@ AS
   DECLARE VARIABLE TOTAL  Double precision = 0;
   DECLARE VARIABLE STR VARCHAR(15);
 BEGIN
-  CONTA = '';
-  ORDEM = 0;
-  TOTAL = 0;
+  CONTA     = '';
+  ORDEM     = 0;
+  TOTAL     = 0;
+  CREDITO   = 0;
+  DEBITO    = 0;
   
   /* Buscar Caixa/Banco */ 
   CONTA = 'CAIXA E BANCO ';
   select valor from SP_MOV_CAIXA(:PDTA2, :PDTA2, 0) where ordem = 0
     INTO :TOTAL;
   
-  CREDITO = CREDITO + TOTAL; 
+  CREDITO = TOTAL; 
+  if (CREDITO IS NULL) THEN 
+    CREDITO = 0;
   suspend; 
   
   /* Buscar Duplicatas a Receber */ 
@@ -25,16 +29,23 @@ BEGIN
   Select sum(r.valor_resto) from RECEBIMENTO r where r.EMISSAO <= :PDTA2 AND ((r.STATUS = '5-') or (r.STATUS = '6-'))
     into :TOTAL;
   
-  CREDITO = CREDITO + TOTAL; 
+  CREDITO = TOTAL; 
+  if (CREDITO IS NULL) THEN 
+    CREDITO = 0;
+
   suspend;  
   
   /* Buscar Estoque */
+  -- Criar um parametro para empresa que não usa estoque.
   CONTA = 'MERCADORIAS ';
   Select sum(VALORCUSTO*SALDOFIM) from SPESTOQUEPRODUTO(:PDTA1, :PDTA2,0,500000, 'TODOS OS GRUPOS CADASTRADOS',
     'TODOS SUBGRUPOS DO CADASTRO', 'TODAS AS MARCAS CADASTRADAS', 0) 
     INTO :TOTAL;
    
-  CREDITO = CREDITO + TOTAL; 
+  CREDITO = TOTAL; 
+  if (CREDITO IS NULL) THEN 
+    CREDITO = 0;
+ 
   suspend;
   
   /* Buscar Imobilizado */  
@@ -48,7 +59,10 @@ BEGIN
     Select sum(m.VALORDEBITO - m.VALORCREDITO) from MOVIMENTOCONT m Where m.CONTA = :Str and m.DATA <= :PDta2
       into :TOTAL;  
   
-    CREDITO = CREDITO + TOTAL; 
+    CREDITO = TOTAL; 
+    if (CREDITO IS NULL) THEN 
+      CREDITO = 0;
+    
     suspend;
   end 
   
@@ -57,7 +71,10 @@ BEGIN
   Select sum(p.valor_resto) from PAGAMENTO p where p.EMISSAO <= :PDTA2 AND ((p.STATUS = '5-') or (p.STATUS = '6-'))
     into :TOTAL;
   
-  DEBITO = DEBITO + TOTAL; 
+  DEBITO = TOTAL; 
+  if (DEBITO IS NULL) THEN 
+    DEBITO = 0;
+  
   suspend;
   
   /* Buscar Emprestimos e Financiamentos */  
@@ -68,10 +85,13 @@ BEGIN
     
   if (str is not null) then    
   begin     
-    Select sum(m.VALORDEBITO - m.VALORCREDITO) from MOVIMENTOCONT m Where m.CONTA = :Str and m.DATA <= :PDta2
+    Select sum(m.VALORCREDITO - m.VALORDEBITO) from MOVIMENTOCONT m Where PLNCTAMAIN(m.CONTA) = :Str and m.DATA <= :PDta2
       into :TOTAL;  
   
-    DEBITO = DEBITO + TOTAL; 
+    DEBITO = TOTAL; 
+    if (DEBITO IS NULL) THEN 
+      DEBITO = 0;
+    
     suspend;
   end 
   
@@ -86,7 +106,10 @@ BEGIN
     Select sum(m.VALORDEBITO - m.VALORCREDITO) from MOVIMENTOCONT m Where m.CONTA = :Str and m.DATA <= :PDta2
       into :TOTAL;  
   
-    DEBITO = DEBITO + TOTAL; 
+    DEBITO = TOTAL; 
+    if (DEBITO IS NULL) THEN 
+      DEBITO = 0;
+    
     suspend;
   end 
   
@@ -101,7 +124,10 @@ BEGIN
     Select sum(m.VALORDEBITO - m.VALORCREDITO) from MOVIMENTOCONT m Where m.CONTA = :Str and m.DATA <= :PDta2
       into :TOTAL;  
   
-    DEBITO = DEBITO + TOTAL; 
+    DEBITO = TOTAL; 
+    if (DEBITO IS NULL) THEN 
+      DEBITO = 0;
+    
     suspend;
   end 
 
