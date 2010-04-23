12abbd7efcb00793188212e0062bce587596189b
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2010-04-23 12:13:27 +0000

    corrigido a exibição da fatura.

--- script/nfe_fatura.sql
+++ script/nfe_fatura.sql
@@ -1,5 +1,3 @@
-SET TERM ^ ;
-
 CREATE or alter PROCEDURE NFE_FATURA 
  ( CODVENDA INTEGER ) 
 RETURNS 
@@ -11,6 +9,9 @@ DECLARE VARIABLE ve_Prazo char(10);
 DECLARE VARIABLE Prazo char(20);
 DECLARE VARIABLE NF char(20);
 DECLARE VARIABLE serie char(10);
+DECLARE VARIABLE VLR DOUBLE PRECISION;
+DECLARE VARIABLE parc smallint;
+
 BEGIN
   /* Gera as FAtura para imprimir na NFe */ 
   select prazo, cast(notafiscal as varchar(15)), serie from venda where codvenda = :codvenda
@@ -19,20 +20,27 @@ BEGIN
   into :ve_prazo;
   if (ve_prazo = '999') then
   begin 
-    for select r.DATAVENCIMENTO, r.VALOR_RESTO, r.TITULO
+    for select r.DATAVENCIMENTO, r.VALOR_RESTO,  r.VIA
       from RECEBIMENTO r where r.TITULO = udf_trim(:NF) || '-' || :Serie
       into :datafatura, :valor, :numerofatura
       do begin
+        numerofatura = udf_trim(:NF) || '/' || UDF_TRIM(:NumeroFatura);
         suspend;
       end
   end
   else begin 
-    select first 1 r.DATAVENCIMENTO, r.VALOR_RESTO, r.TITULO
-      from RECEBIMENTO r where r.TITULO = udf_trim(:NF) || '-' || :Serie
-      into :datafatura, :valor, :numerofatura;
-        suspend;
-    
-  end      
-END^
+    select first 1 r.VALOR_RESTO, parcelas from RECEBIMENTO r  where r.TITULO = udf_trim(:NF) || '-' || :Serie
+      into :vlr, :parc;
 
-SET TERM ; ^
+    for select r.DATAVENCIMENTO, r.via
+      from RECEBIMENTO r  where r.TITULO = udf_trim(:NF) || '-' || :Serie
+      into :datafatura, :numerofatura
+    do begin
+        valor = vlr;
+        if (parc > 0) then
+           valor = vlr / parc; 
+         numerofatura = udf_trim(:NF) || '/' || UDF_TRIM(:NumeroFatura);
+        suspend;
+    end
+  end      
+END
