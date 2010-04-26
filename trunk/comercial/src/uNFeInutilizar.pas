69fdd1efa34e47ad1abfb70979ffbb8e1a616748
ats.manoel <ats.manoel@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2010-04-26 12:52:48 +0000

    adição dos dados da empresa no form

--- trunk/comercial/src/uNFeInutilizar.pas
+++ trunk/comercial/src/uNFeInutilizar.pas
@@ -5,7 +5,8 @@ interface
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, StdCtrls, ExtCtrls, Mask, JvExMask, JvToolEdit, Buttons,
-  MMJPanel, ACBrNFe, xmldom, XMLIntf, msxmldom, XMLDoc;
+  MMJPanel, ACBrNFe, xmldom, XMLIntf, msxmldom, XMLDoc, FMTBcd, DB,
+  DBClient, Provider, SqlExpr, Grids, DBGrids, JvExDBGrids, JvDBGrid;
 
 type
   TfNFeInutilizar = class(TForm)
@@ -25,8 +26,57 @@ type
     btnInutilizar: TButton;
     btnSair: TButton;
     MemoResp: TMemo;
+    ComboBox1: TComboBox;
+    Label7: TLabel;
+    MMJPanel2: TMMJPanel;
+    JvDBGrid1: TJvDBGrid;
+    s_2: TSQLDataSet;
+    s_2CODIGO: TIntegerField;
+    s_2CONTA: TStringField;
+    s_2NOME: TStringField;
+    d_2: TDataSetProvider;
+    cds_ccusto: TClientDataSet;
+    cds_ccustoCODIGO: TIntegerField;
+    cds_ccustoCONTA: TStringField;
+    cds_ccustoNOME: TStringField;
+    sEmpresa: TSQLDataSet;
+    sEmpresaEMPRESA: TStringField;
+    sEmpresaRAZAO: TStringField;
+    sEmpresaCNPJ_CPF: TStringField;
+    sEmpresaENDERECO: TStringField;
+    sEmpresaLOGRADOURO: TStringField;
+    sEmpresaBAIRRO: TStringField;
+    sEmpresaCIDADE: TStringField;
+    sEmpresaUF: TStringField;
+    sEmpresaCEP: TStringField;
+    sEmpresaDDD: TStringField;
+    sEmpresaFONE: TStringField;
+    sEmpresaFONE_1: TStringField;
+    sEmpresaFONE_2: TStringField;
+    sEmpresaFAX: TStringField;
+    sEmpresaE_MAIL: TStringField;
+    sEmpresaWEB: TStringField;
+    sEmpresaLOGOTIPO: TGraphicField;
+    sEmpresaCODIGO: TIntegerField;
+    sEmpresaTIPO: TStringField;
+    sEmpresaIE_RG: TStringField;
+    sEmpresaSLOGAN: TStringField;
+    sEmpresaOUTRAS_INFO: TStringField;
+    sEmpresaCODIGO_CONTA: TIntegerField;
+    sEmpresaDIVERSOS1: TStringField;
+    sEmpresaDIVERSOS2: TStringField;
+    sEmpresaDIVERSOS3: TStringField;
+    sEmpresaANOLETIVO: TIntegerField;
+    sEmpresaMEDIA_ESCOLA: TFloatField;
+    sEmpresaPORTA: TIntegerField;
+    sEmpresaSMTP: TStringField;
+    sEmpresaSENHA: TStringField;
+    sEmpresaNUMERO: TStringField;
+    sEmpresaCCUSTO: TIntegerField;
+    sEmpresaCD_IBGE: TStringField;
     procedure btnInutilizarClick(Sender: TObject);
     procedure btnSairClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -38,7 +88,7 @@ var
 
 implementation
 
-uses uNFeletronica;
+uses uNFeletronica, UDm;
 
 {$R *.dfm}
 
@@ -61,13 +111,49 @@ end;
 
 procedure TfNFeInutilizar.btnInutilizarClick(Sender: TObject);
 begin
- fNFeletronica.ACBrNFe1.WebServices.Inutiliza(RemoveChar(fNFeletronica.sEmpresaCNPJ_CPF.AsString), edtJustificativa.text, StrToInt(edtAno.text), StrToInt(edtModelo.Text), StrToInt(edtSerie.Text), StrToInt(edtNumIni.Text), StrToInt(edtNumFim.Text));
+
+  if (not cds_ccusto.Active) then
+    cds_ccusto.Open;
+  cds_ccusto.Locate('NOME', ComboBox1.Text,[loCaseInsensitive]);
+
+  //Seleciona Empresa de acordo com o CCusto selecionado
+  if (sEmpresa.Active) then
+    sEmpresa.Close;
+  sEmpresa.Params[0].AsInteger := cds_ccustoCODIGO.AsInteger;
+  sEmpresa.Open;
+
+  //verifica se o CC foi selecionado caso não da mensagem avisando
+  if(sEmpresa.IsEmpty) then
+    MessageDlg('Centro de custo não selecionado', mtError, [mbOK], 0);
+
+ fNFeletronica.ACBrNFe1.WebServices.Inutiliza(RemoveChar(sEmpresaCNPJ_CPF.AsString), edtJustificativa.text, StrToInt(edtAno.text), StrToInt(edtModelo.Text), StrToInt(edtSerie.Text), StrToInt(edtNumIni.Text), StrToInt(edtNumFim.Text));
  MemoResp.Lines.Text :=  UTF8Encode(fNFeletronica.ACBrNFe1.WebServices.Inutilizacao.RetWS);
 end;
 
 procedure TfNFeInutilizar.btnSairClick(Sender: TObject);
 begin
   fNFeInutilizar.Close;
+end;
+
+procedure TfNFeInutilizar.FormCreate(Sender: TObject);
+begin
+    if dm.cds_parametro.Active then
+      dm.cds_parametro.Close;
+    dm.cds_parametro.Params[0].AsString := 'CENTRORECEITA';
+    dm.cds_parametro.Open;
+    conta_local := dm.cds_parametroDADOS.AsString;
+    dm.cds_parametro.Close;
+    if cds_ccusto.Active then
+      cds_ccusto.Close;
+    cds_ccusto.Params[0].AsString := conta_local;
+    cds_ccusto.Open;
+    // populo a combobox
+    cds_ccusto.First;
+    while not cds_ccusto.Eof do
+    begin
+      ComboBox1.Items.Add(cds_ccustoNOME.AsString);
+      cds_ccusto.Next;
+    end;
 end;
 
 end.
