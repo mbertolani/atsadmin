ab5dc7f1c6bc7a01f12adf13b1930d4e3abe5f69
ats.manoel <ats.manoel@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2010-05-06 16:50:57 +0000

    adição de relatório simples de estoque com totais

--- trunk/comercial/src/uRelestoque.pas
+++ trunk/comercial/src/uRelestoque.pas
@@ -16,6 +16,7 @@ type
     cbTipo: TComboBox;
     Label1: TLabel;
     RadioButton3: TRadioButton;
+    RadioButton4: TRadioButton;
     procedure BitBtn1Click(Sender: TObject);
     procedure BitBtn2Click(Sender: TObject);
   private
@@ -41,6 +42,8 @@ begin
     fFiltroEstoque.RepRelItem.FileName := str_relatorio + 'estoqueproduto1.rep';
   if (RadioButton3.Checked = True) then
     fFiltroEstoque.RepRelItem.FileName := str_relatorio + 'estoqueprodutoVenda.rep';
+  if (RadioButton4.Checked = True) then
+    fFiltroEstoque.RepRelItem.FileName := str_relatorio + 'lista_estoque.rep';
 
   fFiltroEstoque.repRelItem.Report.DatabaseInfo.Items[0].SQLConnection := dm.sqlsisAdimin;
   if (RadioButton1.Checked = True) then
@@ -54,65 +57,79 @@ begin
     fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PDTA2').Value := StrToDate(fFiltroEstoque.medta2.Text);
   end;
 
-  IF (fFiltroEstoque.Edit5.Text <> '') then
+  if (RadioButton4.Checked = True) then
   begin
-    fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PROD1').Value := fFiltroEstoque.Edit5.Text;
-    IF (fFiltroEstoque.Edit6.Text <> '') then
-       fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PROD2').Value := fFiltroEstoque.Edit6.Text
+    fFiltroEstoque.RepRelItem.Report.Params.ParamByName('DTA1').Value := StrToDate(fFiltroEstoque.medta1.Text);
+    fFiltroEstoque.RepRelItem.Report.Params.ParamByName('DTA2').Value := StrToDate(fFiltroEstoque.medta2.Text);
+    if (fFiltroEstoque.ComboBox4.Text <> '') then
+    begin
+       fFiltroEstoque.RepRelItem.Report.Params.ParamByName('MARCA').Value := fFiltroEstoque.ComboBox4.Text;
+    end
     else
+       fFiltroEstoque.RepRelItem.Report.Params.ParamByName('MARCA').Value := 'TODAS AS MARCAS CADASTRADAS';
+  end
+  ELSE
+  BEGIN
+    IF (fFiltroEstoque.Edit5.Text <> '') then
+    begin
+      fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PROD1').Value := fFiltroEstoque.Edit5.Text;
+      IF (fFiltroEstoque.Edit6.Text <> '') then
+         fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PROD2').Value := fFiltroEstoque.Edit6.Text
+      else
+        fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PROD2').Value := 32000;
+    end else begin
+      fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PROD1').Value := 0;
       fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PROD2').Value := 32000;
-  end else begin
-    fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PROD1').Value := 0;
-    fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PROD2').Value := 32000;
-  end;
-
-  IF (fFiltroEstoque.Edit3.Text <> '') then
-  begin
-    fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PROD1').Value := fFiltroEstoque.Edit3.Text;
-    fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PROD2').Value := fFiltroEstoque.Edit3.Text
-  end;
-
-  if (fFiltroEstoque.cbGRUPO.Text <> '') then
-  begin
-     fFiltroEstoque.RepRelItem.Report.Params.ParamByName('GRUPO').Value := fFiltroEstoque.cbGRUPO.Text;
-  end
-  else
-    fFiltroEstoque.RepRelItem.Report.Params.ParamByName('GRUPO').Value := 'TODOS OS GRUPOS CADASTRADOS';
-
-  if (fFiltroEstoque.cbPRODUTO.Text <> '') then
-  begin
-     fFiltroEstoque.RepRelItem.Report.Params.ParamByName('SUBGRUPO').Value := fFiltroEstoque.cbPRODUTO.Text;
-  end
-  else
-     fFiltroEstoque.RepRelItem.Report.Params.ParamByName('SUBGRUPO').Value := 'TODOS SUBGRUPOS DO CADASTRO';
-
-  if (fFiltroEstoque.ComboBox4.Text <> '') then
-  begin
-     fFiltroEstoque.RepRelItem.Report.Params.ParamByName('MARCA').Value := fFiltroEstoque.ComboBox4.Text;
-  end
-  else
-     fFiltroEstoque.RepRelItem.Report.Params.ParamByName('MARCA').Value := 'TODAS AS MARCAS CADASTRADAS';
-
-
-  if (fFiltroEstoque.ComboBox1.Text <> 'TODOS') then
-  begin
-    dm.cds_ccusto.Locate('NOME', fFiltroEstoque.ComboBox1.Text, [loCaseInsensitive]);
-    fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PCCUSTO').Value := dm.cds_ccustoCODIGO.asInteger;
-  end
-  else
-     fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PCCUSTO').Value := 0;
-
-  if (cbTipo.Text <> '') then
-  begin
-    case cbTipo.ItemIndex of
-      0: fFiltroEstoque.RepRelItem.Report.Params.ParamByName('TIPOPRODUTO').Value := 'TODOSPROD';
-      1: fFiltroEstoque.RepRelItem.Report.Params.ParamByName('TIPOPRODUTO').Value := 'COMPRA';
-      2: fFiltroEstoque.RepRelItem.Report.Params.ParamByName('TIPOPRODUTO').Value := 'VENDA';
-      3: fFiltroEstoque.RepRelItem.Report.Params.ParamByName('TIPOPRODUTO').Value := 'SERV';
-      4: fFiltroEstoque.RepRelItem.Report.Params.ParamByName('TIPOPRODUTO').Value := 'COMPRA';
-      5: fFiltroEstoque.RepRelItem.Report.Params.ParamByName('TIPOPRODUTO').Value := 'VENDA';
     end;
-  end;
+
+    IF (fFiltroEstoque.Edit3.Text <> '') then
+    begin
+      fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PROD1').Value := fFiltroEstoque.Edit3.Text;
+      fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PROD2').Value := fFiltroEstoque.Edit3.Text
+    end;
+
+    if (fFiltroEstoque.cbGRUPO.Text <> '') then
+    begin
+       fFiltroEstoque.RepRelItem.Report.Params.ParamByName('GRUPO').Value := fFiltroEstoque.cbGRUPO.Text;
+    end
+    else
+      fFiltroEstoque.RepRelItem.Report.Params.ParamByName('GRUPO').Value := 'TODOS OS GRUPOS CADASTRADOS';
+
+    if (fFiltroEstoque.cbPRODUTO.Text <> '') then
+    begin
+       fFiltroEstoque.RepRelItem.Report.Params.ParamByName('SUBGRUPO').Value := fFiltroEstoque.cbPRODUTO.Text;
+    end
+    else
+       fFiltroEstoque.RepRelItem.Report.Params.ParamByName('SUBGRUPO').Value := 'TODOS SUBGRUPOS DO CADASTRO';
+
+    if (fFiltroEstoque.ComboBox4.Text <> '') then
+    begin
+       fFiltroEstoque.RepRelItem.Report.Params.ParamByName('MARCA').Value := fFiltroEstoque.ComboBox4.Text;
+    end
+    else
+       fFiltroEstoque.RepRelItem.Report.Params.ParamByName('MARCA').Value := 'TODAS AS MARCAS CADASTRADAS';
+
+
+    if (fFiltroEstoque.ComboBox1.Text <> 'TODOS') then
+    begin
+      dm.cds_ccusto.Locate('NOME', fFiltroEstoque.ComboBox1.Text, [loCaseInsensitive]);
+      fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PCCUSTO').Value := dm.cds_ccustoCODIGO.asInteger;
+    end
+    else
+       fFiltroEstoque.RepRelItem.Report.Params.ParamByName('PCCUSTO').Value := 0;
+
+    if (cbTipo.Text <> '') then
+    begin
+      case cbTipo.ItemIndex of
+        0: fFiltroEstoque.RepRelItem.Report.Params.ParamByName('TIPOPRODUTO').Value := 'TODOSPROD';
+        1: fFiltroEstoque.RepRelItem.Report.Params.ParamByName('TIPOPRODUTO').Value := 'COMPRA';
+        2: fFiltroEstoque.RepRelItem.Report.Params.ParamByName('TIPOPRODUTO').Value := 'VENDA';
+        3: fFiltroEstoque.RepRelItem.Report.Params.ParamByName('TIPOPRODUTO').Value := 'SERV';
+        4: fFiltroEstoque.RepRelItem.Report.Params.ParamByName('TIPOPRODUTO').Value := 'COMPRA';
+        5: fFiltroEstoque.RepRelItem.Report.Params.ParamByName('TIPOPRODUTO').Value := 'VENDA';
+      end;
+    end;
+  END;
 
   fFiltroEstoque.RepRelItem.Title := fFiltroEstoque.RepRelItem.FileName;
   fFiltroEstoque.RepRelItem.Execute;
