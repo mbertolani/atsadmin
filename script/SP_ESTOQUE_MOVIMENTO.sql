d02d8b282efe7f0979d78e701aada97a43eebcb3
ats.manoel <ats.manoel@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2010-05-13 17:49:04 +0000

    Estoque simples de entradas e saidas

---
+++ script/SP_ESTOQUE_MOVIMENTO.sql
@@ -0,0 +1,44 @@
+SET TERM ^;
+CREATE OR ALTER PROCEDURE SP_ESTOQUE_MOVIMENTO (
+    DTA1 Date,
+    DTA2 Date,
+    MARC Varchar(50) )
+RETURNS (
+    CODPRO Varchar(20),
+    CODPRODUTO INTEGER,
+    PRODUTO Varchar(200),
+    EST_ATUAL Double precision,
+    ENTRADA Double precision,
+    SAIDA Double precision,
+    PRECOMEDIO Double precision,
+    UN CHAR(2),
+    MARCA Varchar(50))
+AS
+
+BEGIN
+
+        FOR SELECT prod.CODPRODUTO, prod.PRODUTO, prod.ESTOQUEATUAL, prod.PRECOMEDIO, 
+        prod.CODPRO, prod.UNIDADEMEDIDA, prod.MARCA
+        FROM PRODUTOS prod 
+        WHERE ((prod.MARCA = :MARC) OR (:MARC = 'TODAS AS MARCAS CADASTRADAS'))
+        INTO :CODPRODUTO, :PRODUTO, :EST_ATUAL , :PRECOMEDIO, :CODPRO, :UN, :MARCA
+
+        DO BEGIN
+            --COMPRA / ENTRADA
+            SELECT SUM(md.QUANTIDADE) as QUANTIDADE FROM MOVIMENTODETALHE md
+            INNER JOIN MOVIMENTO m on m.CODMOVIMENTO = md.CODMOVIMENTO 
+            where md.CODPRODUTO = :CODPRODUTO and md.BAIXA = 0 and (m.DATAMOVIMENTO BETWEEN :DTA1 AND :DTA2)
+            into :ENTRADA;
+            
+            --VENDA / SAIDA
+            SELECT SUM(md.QUANTIDADE) as QUANTIDADE FROM MOVIMENTODETALHE md
+            INNER JOIN MOVIMENTO m on m.CODMOVIMENTO = md.CODMOVIMENTO 
+            where md.CODPRODUTO = :CODPRODUTO and md.BAIXA = 1 and (m.DATAMOVIMENTO BETWEEN :DTA1 AND :DTA2)
+            into :SAIDA;
+            
+            SUSPEND;
+ 
+    END
+
+END
+
