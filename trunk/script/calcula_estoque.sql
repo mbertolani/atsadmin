703524024f2efb6adb58558f54ca30ba0f75ccc6
ats.manoel <ats.manoel@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2010-05-13 13:48:49 +0000

    correção da procura do estoque

--- trunk/script/calcula_estoque.sql
+++ trunk/script/calcula_estoque.sql
@@ -1,16 +1,14 @@
+SET TERM ^ ;
 CREATE OR ALTER PROCEDURE CALC_ESTOQUE (
-    DATAINI DATE,
-    DATAFIM DATE)
-    
+    DATAINI Date,
+    DATAFIM Date )
 RETURNS (
-    CODPRO INTEGER,
+    CODPRO Integer,
     DESCPRO Varchar(300),
     QNT Double precision,
     ESTOQUE Double precision,
-    SALDO DOUBLE PRECISION )
-    
+    SALDO Double precision )
 AS
-DECLARE VARIABLE CODMOVDET INTEGER;
 
 BEGIN
     FOR SELECT md.CODPRODUTO, SUM(md.QUANTIDADE), md.DESCPRODUTO
@@ -19,9 +17,18 @@ BEGIN
     where md.BAIXA is null and m.CODNATUREZA = 3 and m.DATAMOVIMENTO BETWEEN :DATAINI and :DATAFIM
     group by md.CODPRODUTO, md.DESCPRODUTO
     into :CODPRO, :QNT, :DESCPRO
-    do begin
-        select p.ESTOQUEATUAL from PRODUTOS p where p.CODPRO = :CODPRO
-        INTO :ESTOQUE;
+    do begin    
+    select sum(p.SALDOFIM) from SPESTOQUEPRODUTO(:DATAINI
+                                 , :DATAFIM
+                                 , :CODPRO
+                                 , :CODPRO
+                                 , 'TODOS OS GRUPOS CADASTRADOS'
+                                 , 'TODOS SUBGRUPOS DO CADASTRO'
+                                 , 'TODAS AS MARCAS CADASTRADAS'
+                                 , 0) p
+        into :ESTOQUE;
+  --      select p.ESTOQUEATUAL from PRODUTOS p where p.CODPRO = :CODPRO
+    --    INTO :ESTOQUE;
         SALDO = :ESTOQUE - :QNT;
         SUSPEND;
     end
