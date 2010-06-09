11f9ca501f5ceb4ee0c5b204fb67afd76f9d4e2e
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2010-06-09 21:56:15 +0000

    Corrigido qdo nao usa prazo.

--- script/nfe_fatura.sql
+++ script/nfe_fatura.sql
@@ -1,11 +1,11 @@
-set term ^ ;
-CREATE or alter PROCEDURE NFE_FATURA 
- ( CODVENDA INTEGER ) 
-RETURNS 
- ( DATAFATURA DATE,
-   VALOR DOUBLE PRECISION,
-   NUMEROFATURA VARCHAR(20)  )
-AS 
+SET TERM ^ ;
+ALTER PROCEDURE NFE_FATURA (
+    CODVENDA Integer )
+RETURNS (
+    DATAFATURA Date,
+    VALOR Double precision,
+    NUMEROFATURA Varchar(20) )
+AS
 DECLARE VARIABLE ve_Prazo char(10); 
 DECLARE VARIABLE Prazo char(20);
 DECLARE VARIABLE NF char(20);
@@ -14,20 +14,36 @@ DECLARE VARIABLE VLR DOUBLE PRECISION;
 DECLARE VARIABLE parc smallint;
 
 BEGIN
+  parc = 0; 
   /* Gera as FAtura para imprimir na NFe */ 
   select prazo, cast(notafiscal as varchar(15)), serie from venda where codvenda = :codvenda
   into :prazo, :nf, :serie; 
   select D9 from parametro where parametro = :prazo
   into :ve_prazo;
+  if (prazo is null) then 
+  begin
+    for select r.DATAVENCIMENTO, r.VALOR_RESTO,  r.VIA
+      from RECEBIMENTO r where r.TITULO = udf_trim(:NF) || '-' || :Serie order by r.DATAVENCIMENTO, r.VIA
+      into :datafatura, :valor, :numerofatura
+      do begin
+        parc = parc + 1;
+        numerofatura = udf_trim(:NF) || '/' || UDF_TRIM(:parc);
+        --numerofatura = udf_trim(:NF) || '/' || UDF_TRIM(:NumeroFatura);
+        suspend;
+      end     
+  end
+  ELSE
+  BEGIN
   if (ve_prazo = '999') then
   begin 
+    parc = parc + 1;
     for select r.DATAVENCIMENTO, r.VALOR_RESTO,  r.VIA
 
       from RECEBIMENTO r where r.TITULO = udf_trim(:NF) || '-' || :Serie order by r.DATAVENCIMENTO, r.VIA
 
       into :datafatura, :valor, :numerofatura
       do begin
-        numerofatura = udf_trim(:NF) || '/' || UDF_TRIM(:NumeroFatura);
+        numerofatura = udf_trim(:NF) || '/' || UDF_TRIM(:parc);
         suspend;
       end
   end
@@ -46,15 +62,12 @@ BEGIN
         numerofatura = udf_trim(:NF) || '/' || UDF_TRIM(:parc);
         suspend;
     end
-  end 
-  if (prazo is null) then 
-  begin
-    for select r.DATAVENCIMENTO, r.VALOR_RESTO,  r.VIA
-      from RECEBIMENTO r where r.CODVENDA = :codvenda order by r.DATAVENCIMENTO, r.VIA
-      into :datafatura, :valor, :numerofatura
-      do begin
-        numerofatura = udf_trim(:NF) || '/' || UDF_TRIM(:NumeroFatura);
-        suspend;
-      end     
-  end      
-END
+  end
+  END    
+END^
+SET TERM ; ^
+
+
+GRANT EXECUTE
+ ON PROCEDURE NFE_FATURA TO  SYSDBA;
+
