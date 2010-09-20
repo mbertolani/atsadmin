4e57786ff51e6190656b8e40ba70371dc32294af
douglas.darte <douglas.darte@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2010-09-20 16:45:22 +0000

    carrega CFOP no detalhe quando for compra

--- trunk/script/trg_cfop_produtos.sql
+++ trunk/script/trg_cfop_produtos.sql
@@ -1,18 +1,25 @@
-SET TERM ^ ;
-ALTER TRIGGER CFOP_PRODUTOS ACTIVE
+CREATE OR ALTER TRIGGER CFOP_PRODUTOS FOR NOTAFISCAL ACTIVE
 AFTER INSERT OR UPDATE POSITION 0
 AS 
 declare variable codm INTEGER;
 BEGIN
-    select v.CODMOVIMENTO from venda v
-    where v.CODVENDA = new.CODVENDA
-    into :codm;
-    
+    if (new.NATUREZA = 15) then
+    begin
+        select v.CODMOVIMENTO from venda v
+        where v.CODVENDA = new.CODVENDA
+        into :codm;
+    end
+    else
+    begin
+        select c.CODMOVIMENTO from COMPRA c
+        where c.CODCOMPRA = new.CODVENDA
+        into :codm;
+    end
+        
 	if (inserting) then
 	 update MOVIMENTODETALHE set CFOP = new.CFOP where CODMOVIMENTO = :codm;
 	else
      if(old.CFOP <> new.CFOP) then
-        update MOVIMENTODETALHE set CFOP = new.CFOP
+        update MOVIMENTODETALHE set CFOP = udf_left(new.CFOP, 4)
         where CODMOVIMENTO = :codm;
-END^
-SET TERM ; ^
+END
