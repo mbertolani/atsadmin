f07fd298aefe7cf00966d355d8791d8235f8b5f5
douglas.darte <douglas.darte@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2010-10-15 14:41:08 +0000

    correção para qnd tiver entrada

--- trunk/script/corrige_valor_fatura.sql
+++ trunk/script/corrige_valor_fatura.sql
@@ -2,8 +2,10 @@ CREATE TRIGGER CORRIGE_VALOR_FATURA FOR RECEBIMENTO ACTIVE
 BEFORE UPDATE POSITION 0
 AS 
 BEGIN 
-    if (old.VALOR_RESTO_SST is null) then
+    if ((old.VALOR_RESTO_SST is null) and (new.STATUS = '5-') )then
         new.VALOR_RESTO_SST = old.VALOR_RESTO;
+    if ((old.VALOR_RESTO_SST is null) and (new.STATUS = '7-') )then
+        new.VALOR_RESTO_SST = old.VALORRECEBIDO;
     if (new.VALST >= 0) then
         new.VALOR_RESTO = (new.VALST + new.VALOR_RESTO_SST);
 END
