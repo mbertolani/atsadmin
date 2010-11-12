76957199b91f98242edbc0048f3155e2e0533fef
crsilveira@gmail.com <crsilveira@gmail.com@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2010-11-12 18:55:57 +0000

    Proc. e Triger para o Inventario.

---
+++ script/invent_estoque.sql
@@ -0,0 +1,20 @@
+SET TERM ^ ;
+
+CREATE OR ALTER TRIGGER INVENT_ESTOQUE FOR INVENTARIO
+ACTIVE BEFORE INSERT POSITION 0
+AS 
+declare variable qtde double precision;
+BEGIN 
+  if (inserting) then 
+  begin 
+	-- Preenche o campo ESTOQUE_ATUAL
+    -- Ve o estoque atual
+    select SALDOINI from SPESTOQUE (NEW.DATAIVENTARIO, NEW.DATAIVENTARIO, NEW.CODPRODUTO, NEW.CODPRODUTO, 
+       'TODOS OS GRUPOS CADASTRADOS NO SISTEMA', 
+       'TODOS OS SUBGRUPOS CADASTRADOS NO SISTEMA')
+    into :qtde;   
+    NEW.ESTOQUE_ATUAL = :qtde;    
+  end 
+END^
+
+SET TERM ; ^ 
