29493976f6188af40c00d12218a1b71cd5ca418d
douglas.darte <douglas.darte@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2010-11-30 12:01:05 +0000

    correção para alterar primeiro titulo

--- comercial/src/ufCrAltera.pas
+++ comercial/src/ufCrAltera.pas
@@ -180,6 +180,8 @@ type
     DBEdit6: TDBEdit;
     Label8: TLabel;
     JvLabel1: TJvLabel;
+    sdsVALOR_RESTO_SST: TFloatField;
+    cdsVALOR_RESTO_SST: TFloatField;
     procedure btnGravarClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure btnIncluirClick(Sender: TObject);
@@ -192,6 +194,9 @@ type
     procedure cdsReconcileError(DataSet: TCustomClientDataSet;
       E: EReconcileError; UpdateKind: TUpdateKind;
       var Action: TReconcileAction);
+    procedure DBEdit5Change(Sender: TObject);
+    procedure FormKeyPress(Sender: TObject; var Key: Char);
+    procedure DBEdit5Exit(Sender: TObject);
   private
     { Private declarations }
   public
@@ -568,6 +573,30 @@ begin
   inherited;
   MessageDlg('Não é possível gravar o registro. Erro : ' + E.Message , mtWarning,
         [mbOk], 0);
+end;
+
+procedure TfCrAltera.DBEdit5Change(Sender: TObject);
+begin
+//  inherited;
+  if (cds.State in [dsEdit]) then
+    if( cdsVIA.AsString = '1  ') then
+      cdsVALOR_RESTO_SST.AsFloat := cdsVALOR_RESTO.AsFloat;
+end;
+
+procedure TfCrAltera.FormKeyPress(Sender: TObject; var Key: Char);
+begin
+  inherited;
+  if (cds.State in [dsEdit]) then
+    if( cdsVIA.AsString = '1  ') then
+      cdsVALOR_RESTO_SST.AsFloat := cdsVALOR_RESTO.AsFloat;
+end;
+
+procedure TfCrAltera.DBEdit5Exit(Sender: TObject);
+begin
+//  inherited;
+  if (cds.State in [dsEdit]) then
+    if( cdsVIA.AsString = '1  ') then
+      cdsVALOR_RESTO_SST.AsFloat := cdsVALOR_RESTO.AsFloat;
 end;
 
 end.
