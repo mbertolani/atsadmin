adc462f1966f40157d054a266d8afd2f1f9a1f9b
crsilveira@gmail.com <crsilveira@gmail.com@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2010-12-06 21:15:20 +0000

    Corrigido para executar somente qdo o pedido tivesse sido finalizado.

--- trunk/script/apaga_estoque.sql
+++ trunk/script/apaga_estoque.sql
@@ -1,3 +1,4 @@
+set term ^ ;
 CREATE OR ALTER TRIGGER APAGA_ESTOQUE FOR MOVIMENTODETALHE
 AFTER DELETE
 POSITION 0
@@ -6,13 +7,16 @@ as
   DECLARE VARIABLE dataFim date;
   declare variable codP int;
 begin
-  SELECT m.DataMovimento FROM movimento m where m.CODMOVIMENTO = old.CODMOVIMENTO 
-  INTO :dataIni;
+  if (old.baixa is not null) then 
+  begin 
+    SELECT m.DataMovimento FROM movimento m where m.CODMOVIMENTO = old.CODMOVIMENTO 
+    INTO :dataIni;
 
-  select max(m.dataMovimento) from movimento m 
-    inner join MOVIMENTODETALHE md on md.codmovimento = m.CODMOVIMENTO
-    where md.codproduto = old.CODPRODUTO
+    select max(m.dataMovimento) from movimento m 
+      inner join MOVIMENTODETALHE md on md.codmovimento = m.CODMOVIMENTO
+      where md.codproduto = old.CODPRODUTO
     into :dataFim;
 
-  execute PROCEDURE CORRIGEESTOQUE(old.CODPRODUTO, old.CODPRODUTO, :dataIni, :dataFim, 'N');
+    execute PROCEDURE CORRIGEESTOQUE(old.CODPRODUTO, old.CODPRODUTO, :dataIni, :dataFim, 'N');
+  end  
 end
