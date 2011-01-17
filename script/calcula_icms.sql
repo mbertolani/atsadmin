47b2a5f89998a30a82c45fbd4048930d82da9be0
douglas.darte <douglas.darte@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-01-17 17:34:08 +0000

    correção para calcular com frete e outras despesas

--- script/calcula_icms.sql
+++ script/calcula_icms.sql
@@ -25,26 +25,29 @@ declare variable TOTST DOUBLE PRECISION;
 declare variable TOTIPI DOUBLE PRECISION;
 declare variable TOTPROD DOUBLE PRECISION;
 declare variable TOTBASEST DOUBLE PRECISION;
+declare variable SEG DOUBLE PRECISION;
+declare variable VFRETE DOUBLE PRECISION;
+declare variable VOUTROS DOUBLE PRECISION;
 declare variable codCli integer;
 declare variable d9 SMALLINT;
 declare variable nat SMALLINT;
 begin
 
-    select v.codmovimento, v.NOTAFISCAL, v.SERIE, v.CODVENDA, v.prazo, n.NATUREZA from venda v
-        inner join notafiscal n on n.CODVENDA = v.CODVENDA where n.NUMNF = :numero_nf and (n.NATUREZA = 12 or n.NATUREZA = 15)
-    into :cod, :notaFiscalVenda, :serie, :codv, :prazo, :nat; 
+    select v.codmovimento, v.NOTAFISCAL, v.SERIE, v.CODVENDA, v.prazo, n.NATUREZA, n.FRETE, n.OUTRAS_DESP, n.VALOR_SEGURO from venda v
+        inner join notafiscal n on n.CODVENDA = v.CODVENDA where n.NUMNF = :numero_nf and (n.NATUREZA = 12 or n.NATUREZA = 15 or n.NATUREZA = 16)
+    into :cod, :notaFiscalVenda, :serie, :codv, :prazo, :nat, :VFRETE, :SEG, :VOUTROS; 
     	
-    if ((:nat = '') or (:nat = null) or (:nat is null)) then
+    if (nat is null) then
     select c.codmovimento, c.NOTAFISCAL, c.SERIE, c.CODCOMPRA, c.prazo, n.NATUREZA from COMPRA c
-        inner join notafiscal n on n.CODVENDA = c.CODCOMPRA where n.NUMNF = :numero_nf and n.NATUREZA = 20
-    into :cod, :notaFiscalVenda, :serie, :codv, :prazo, :nat;         
+        inner join notafiscal n on n.CODVENDA = c.CODCOMPRA where n.NUMNF = :numero_nf and (n.NATUREZA = 20 or n.NATUREZA = 21)
+    into :cod, :notaFiscalVenda, :serie, :codv, :prazo, :nat;              
 
     select sum(md.ICMS_SUBST), sum(md.ICMS_SUBSTD), sum(md.VALOR_ICMS), sum(md.VLR_BASEICMS), sum(md.VIPI), sum(md.QUANTIDADE * md.VLR_BASE) from MOVIMENTODETALHE md
     where md.CODMOVIMENTO = :cod
     into :TOTST, :TOTBASEST, :TOTICMS, :TOTBASEICMS, :TOTIPI, :TOTPROD;
   
     UPDATE NOTAFISCAL SET BASE_ICMS_SUBST = :TOTBASEST, VALOR_ICMS_SUBST = :TOTST, VALOR_IPI = :TOTIPI, VALOR_ICMS = :TOTICMS, BASE_ICMS = :TOTBASEICMS ,
-      VALOR_TOTAL_NOTA = :TOTPROD + :TOTST + :TOTIPI
+      VALOR_TOTAL_NOTA = :TOTPROD + :TOTST + :TOTIPI + :VFRETE + :SEG + :VOUTROS
       where NUMNF = :numero_nf;
               
   
