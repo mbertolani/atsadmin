b84d3159c1cd2cb14405414f840f44ec1b55c277
douglas.darte <douglas.darte@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-03-07 14:34:29 +0000

    adição de FRETE e DESCONTO no Total da Nota

--- trunk/script/calcula_icms.sql
+++ trunk/script/calcula_icms.sql
@@ -28,26 +28,33 @@ declare variable TOTBASEST DOUBLE PRECISION;
 declare variable SEG DOUBLE PRECISION;
 declare variable VFRETE DOUBLE PRECISION;
 declare variable VOUTROS DOUBLE PRECISION;
+declare variable bcfrete DOUBLE PRECISION;
+declare variable stfrete DOUBLE PRECISION;
+declare variable DESCONTO DOUBLE PRECISION;
 declare variable codCli integer;
 declare variable d9 SMALLINT;
 declare variable nat SMALLINT;
 begin
 
-    select v.codmovimento, v.NOTAFISCAL, v.SERIE, v.CODVENDA, v.prazo, n.NATUREZA, n.FRETE, n.OUTRAS_DESP, n.VALOR_SEGURO from venda v
+    select v.codmovimento, v.NOTAFISCAL, v.SERIE, v.CODVENDA, v.prazo, n.NATUREZA, n.VALOR_FRETE, n.OUTRAS_DESP, n.VALOR_SEGURO, n.VALOR_DESCONTO from venda v
         inner join notafiscal n on n.CODVENDA = v.CODVENDA where n.NUMNF = :numero_nf and (n.NATUREZA = 12 or n.NATUREZA = 15 or n.NATUREZA = 16)
-    into :cod, :notaFiscalVenda, :serie, :codv, :prazo, :nat, :VFRETE, :SEG, :VOUTROS; 
+    into :cod, :notaFiscalVenda, :serie, :codv, :prazo, :nat, :VFRETE, :SEG, :VOUTROS, :DESCONTO; 
     	
     if (nat is null) then
-    select c.codmovimento, c.NOTAFISCAL, c.SERIE, c.CODCOMPRA, c.prazo, n.NATUREZA from COMPRA c
+    select c.codmovimento, c.NOTAFISCAL, c.SERIE, c.CODCOMPRA, c.prazo, n.NATUREZA, n.VALOR_DESCONTO from COMPRA c
         inner join notafiscal n on n.CODVENDA = c.CODCOMPRA where n.NUMNF = :numero_nf and (n.NATUREZA = 20 or n.NATUREZA = 21)
-    into :cod, :notaFiscalVenda, :serie, :codv, :prazo, :nat;              
+    into :cod, :notaFiscalVenda, :serie, :codv, :prazo, :nat, :DESCONTO;              
 
-    select sum(md.ICMS_SUBST), sum(md.ICMS_SUBSTD), sum(md.VALOR_ICMS), sum(md.VLR_BASEICMS), sum(md.VIPI), sum(md.QUANTIDADE * md.VLR_BASE) from MOVIMENTODETALHE md
+    select COALESCE(sum(md.ICMS_SUBST), 0), COALESCE(sum(md.ICMS_SUBSTD), 0), COALESCE(sum(md.VALOR_ICMS), 0), COALESCE(sum(md.VLR_BASEICMS), 0), COALESCE(sum(md.VIPI), 0), COALESCE(sum(md.QUANTIDADE * md.VLR_BASE), 0) from MOVIMENTODETALHE md
     where md.CODMOVIMENTO = :cod
     into :TOTST, :TOTBASEST, :TOTICMS, :TOTBASEICMS, :TOTIPI, :TOTPROD;
-  
-    UPDATE NOTAFISCAL SET BASE_ICMS_SUBST = :TOTBASEST, VALOR_ICMS_SUBST = :TOTST, VALOR_IPI = :TOTIPI, VALOR_ICMS = :TOTICMS, BASE_ICMS = :TOTBASEICMS ,
-      VALOR_TOTAL_NOTA = :TOTPROD + :TOTST + :TOTIPI + :VFRETE + :SEG + :VOUTROS
+    
+    select first 1 COALESCE(md.BCFRETE, 0), COALESCE(md.STFRETE, 0) from MOVIMENTODETALHE md
+    where md.CODMOVIMENTO = :cod
+    into :bcfrete, :stfrete;
+    
+    UPDATE NOTAFISCAL SET BASE_ICMS_SUBST = :TOTBASEST + :BCFRETE, VALOR_ICMS_SUBST = :TOTST + :STFRETE, VALOR_IPI = :TOTIPI, VALOR_ICMS = :TOTICMS, BASE_ICMS = :TOTBASEICMS ,
+      VALOR_TOTAL_NOTA = :TOTPROD + :TOTST + :TOTIPI + :VFRETE + :SEG + :VOUTROS + :STFRETE - :DESCONTO
       where NUMNF = :numero_nf;
               
   
