d934fd9e78b12c563098eea7a359b26acda4e1ac
douglas.darte <douglas.darte@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-04-08 20:11:56 +0000

    correção para adicionar DESCONTO E FRETE em cada produto

--- trunk/script/frete_nf.sql
+++ trunk/script/frete_nf.sql
@@ -3,7 +3,17 @@ AFTER INSERT OR UPDATE POSITION 0
 AS 
 declare variable codm INTEGER;
 declare variable coddet INTEGER;
+declare variable det1 INTEGER;
+declare variable det INTEGER;
+declare variable FRETE double precision;
+declare variable DESCONTO double precision;
+declare variable FRETE_UNIT double precision;
+declare variable DESCONTO_UNIT double precision;
+declare variable FRETE_TOTAL double precision;
+declare variable DESCONTO_TOTAL double precision;
+declare variable valtot double precision;
 BEGIN
+--alter table MOVIMENTODETALHE add VALOR_DESCONTO double precision
     if ((new.NATUREZA = 15) or (new.NATUREZA = 12) or (new.NATUREZA = 16)) then
     begin
         select v.CODMOVIMENTO from venda v
@@ -16,15 +26,43 @@ BEGIN
         where c.CODCOMPRA = new.CODVENDA
         into :codm;
     end
-    
-    select first 1 md.CODDETALHE from MOVIMENTODETALHE md
-    where md.CODMOVIMENTO = :codm
-    order by md.CODDETALHE
-    into :coddet;
-    
+   
+   FRETE = new.VALOR_FRETE;
+   DESCONTO = new.VALOR_DESCONTO;
+   if (frete is null) then
+    frete = 0;
+   if (desconto is null) then
+    desconto = 0; 
+   
+   Frete_UNIT = new.VALOR_FRETE / new.VALOR_PRODUTO;
+   DESCONTO_UNIT = new.VALOR_DESCONTO / new.VALOR_PRODUTO;
+   
+   Frete_TOTAL = new.VALOR_FRETE;
+   DESCONTO_TOTAL = new.VALOR_DESCONTO;
+   
+   select count(md.CODDETALHE) from MOVIMENTODETALHE md
+   where md.CODMOVIMENTO = :codm
+   into :det;
+   det1 = 1;
+   for select md.CODDETALHE, md.VALTOTAL from MOVIMENTODETALHE md
+   where md.CODMOVIMENTO = :codm
+   into :coddet, :valtot
+   do begin
 	if (inserting) then
-	 update MOVIMENTODETALHE set FRETE = new.valor_frete where CODDETALHE = :coddet;
+	begin
+	 update MOVIMENTODETALHE set FRETE = UDF_ROUNDDEC((:Frete_UNIT * :valtot), 2), valor_desconto = UDF_ROUNDDEC((:DESCONTO_UNIT * :valtot), 2) where CODDETALHE = :coddet;
+	 if ( det = det1) then
+	   update MOVIMENTODETALHE set FRETE = :Frete_TOTAL, valor_desconto = :DESCONTO_TOTAL where CODDETALHE = :coddet;
+    end
 	else
      if(old.VALOR_FRETE <> new.VALOR_FRETE) then
-        update MOVIMENTODETALHE set FRETE = new.valor_frete where CODDETALHE = :coddet;
+     begin
+        update MOVIMENTODETALHE set FRETE = UDF_ROUNDDEC((:Frete_UNIT * :valtot), 2), valor_desconto = UDF_ROUNDDEC((:DESCONTO_UNIT * :valtot), 2) where CODDETALHE = :coddet;
+        if ( det = det1) then
+	      update MOVIMENTODETALHE set FRETE = :Frete_TOTAL, valor_desconto = :DESCONTO_TOTAL where CODDETALHE = :coddet;
+    end
+    det1 = det1 +1;
+    Frete_TOTAL   = Frete_TOTAL - UDF_ROUNDDEC((:Frete_UNIT * :valtot), 2);
+    DESCONTO_TOTAL = DESCONTO_TOTAL - UDF_ROUNDDEC((:DESCONTO_UNIT * :valtot), 2);
+    end
 END
