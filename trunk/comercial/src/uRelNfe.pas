8492f505efe94e3751020b8eee54d3ae74bf4458
ats.manoel <ats.manoel@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-04-18 12:37:27 +0000

    form para filtrar Nfe por periodo

---
+++ trunk/comercial/src/uRelNfe.pas
@@ -0,0 +1,43 @@
+unit uRelNfe;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Mask, JvExMask, JvToolEdit, rpcompobase, rpvclreport;
+
+type
+  TfRelNfe = class(TForm)
+    Label5: TLabel;
+    Label7: TLabel;
+    Button1: TButton;
+    Dta1: TJvDateEdit;
+    Dta2: TJvDateEdit;
+    VCLReport1: TVCLReport;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  fRelNfe: TfRelNfe;
+
+implementation
+
+uses UDm;
+
+{$R *.dfm}
+
+procedure TfRelNfe.Button1Click(Sender: TObject);
+begin
+  VCLReport1.Filename := str_relatorio + 'nota_fiscal_rel_emissao.rep';
+  VCLReport1.Title := VCLReport1.Filename;
+  VCLReport1.Report.DatabaseInfo.Items[0].SQLConnection := dm.sqlsisAdimin;
+  VCLReport1.Report.Params.ParamByName('DTAINI').Value := StrToDate(Dta1.Text);
+  VCLReport1.Report.Params.ParamByName('DTAFIM').Value := StrToDate(Dta2.Text);
+  VCLReport1.execute;
+end;
+
+end.
