190550dd1c8e5c87ba705a0291f0968a11e01d1a
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-05-18 21:58:44 +0000

    Finalizando Rotina de Cotação.

--- comercial/src/uCotacoesHist.pas
+++ comercial/src/uCotacoesHist.pas
@@ -134,6 +134,9 @@ begin
    ' a.COTACAO_IPI, a.COTACAO_DESCONTO, a.COTACAO_FRETE' +
    '  FROM COMPRA_COTACAO a ';
   strP := strP + 'where a.COTACAO_FORNEC = ' + IntToStr(ClientDataSet1COTACAO_FORNEC.AsInteger);
+  strP := strP + '  and a.COTACAO_DATA BETWEEN ' +
+    QuotedStr(FormatDateTime('mm/dd/yyyy', dta1.Date)) + ' AND '  +
+    QuotedStr(FormatDateTime('mm/dd/yyyy', dta2.Date));
   if (edProduto.Text <> '') then
   begin
     strP := strP + 'and a.COTACAO_ITEM = ' + QuotedStr(edProduto.Text);
