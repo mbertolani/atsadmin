4cd0c47590644d1263c80953724d1c2fff84c20c
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-06-06 20:39:16 +0000

    Opção de não informar periodo.

--- trunk/comercial/src/uCompraRecebimento.pas
+++ trunk/comercial/src/uCompraRecebimento.pas
@@ -59,6 +59,7 @@ type
     procedure cbMesChange(Sender: TObject);
     procedure JvDBGrid1KeyPress(Sender: TObject; var Key: Char);
     procedure BitBtn1Click(Sender: TObject);
+    procedure btnClienteProcuraClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -70,7 +71,7 @@ var
 
 implementation
 
-uses uUtils, UDm;
+uses uUtils, UDm, uProcurar;
 
 {$R *.dfm}
 
@@ -123,22 +124,39 @@ procedure TfCompraRecebimento.btnProcurarClick(Sender: TObject);
 var str, stra: string;
 begin
   stra := '';
-  str := 'select md.CODDETALHE, md.CODMOVIMENTO, m.DATA_ENTREGA, p.CODPRO, p.PRODUTO' +
-    ', (md.QUANTIDADE - md.RECEBIDO) QUANTIDADE, md.PRECO, md.VALTOTAL , md.RECEBIDO' +
-    ',  m.CONTROLE ' +
-    ' from MOVIMENTODETALHE md ' +
-    ' inner join MOVIMENTO m on  m.CODMOVIMENTO  = md.CODMOVIMENTO ' +
-    ' inner join PRODUTOS   p on  md.CODPRODUTO    = p.CODPRODUTO ' +
-    ' where m.CODNATUREZA   = 5 ' +
-    '   and m.STATUS        = 3 ' +
-    '   and ((md.QUANTIDADE - md.RECEBIDO) > 0) ' +
-    '   and md.RECEBIDO = 0';
 
   if (cdsPedido.Active) then
     cdsPedido.Close;
 
   if (rgStatus.ItemIndex = 0) then
-    stra := stra + ' and md.STATUS is null ';
+  begin
+    str := 'select md.CODDETALHE, md.CODMOVIMENTO, m.DATA_ENTREGA, p.CODPRO, p.PRODUTO' +
+      ', (md.QUANTIDADE - md.RECEBIDO) QUANTIDADE, md.PRECO, md.VALTOTAL , md.RECEBIDO' +
+      ',  m.CONTROLE ' +
+      ' from MOVIMENTODETALHE md ' +
+      ' inner join MOVIMENTO m on  m.CODMOVIMENTO  = md.CODMOVIMENTO ' +
+      ' inner join PRODUTOS   p on  md.CODPRODUTO    = p.CODPRODUTO ' +
+      ' where m.CODNATUREZA   = 5 ';
+
+    stra := stra + ' and ((m.STATUS = 3) OR (m.STATUS = 4)) ';
+    stra := stra + ' and ((md.QUANTIDADE - md.RECEBIDO) > 0) ';
+    stra := stra + ' and md.RECEBIDO = 0';
+    BitBtn1.Enabled := True;
+  end;
+
+  if (rgStatus.ItemIndex = 1) then
+  begin
+    str := 'select md.CODDETALHE, md.CODMOVIMENTO, m.DATA_ENTREGA, p.CODPRO, p.PRODUTO' +
+      ', md.RECEBIDO QUANTIDADE, md.PRECO, md.VALTOTAL , md.RECEBIDO' +
+      ',  m.CONTROLE ' +
+      ' from MOVIMENTODETALHE md ' +
+      ' inner join MOVIMENTO m on  m.CODMOVIMENTO  = md.CODMOVIMENTO ' +
+      ' inner join PRODUTOS   p on  md.CODPRODUTO    = p.CODPRODUTO ' +
+      ' where m.CODNATUREZA   = 5 ';
+
+    stra := stra + ' and m.STATUS = 5';
+    BitBtn1.Enabled := False;
+  end;
 
   if (edFornec.Text <> '') then
   begin
@@ -151,9 +169,12 @@ begin
   end;
 
   // Período
-  stra := stra + ' and m.DATA_ENTREGA BETWEEN ' +
-    QuotedStr(FormatDateTime('mm/dd/yyyy', dta1.Date)) + ' AND '  +
-    QuotedStr(FormatDateTime('mm/dd/yyyy', dta2.Date));
+  if ((dta1.Checked) and (dta2.Checked)) then
+  begin
+    stra := stra + ' and m.DATA_ENTREGA BETWEEN ' +
+      QuotedStr(FormatDateTime('mm/dd/yyyy', dta1.Date)) + ' AND '  +
+      QuotedStr(FormatDateTime('mm/dd/yyyy', dta2.Date));
+  end;
 
   cdsPedido.CommandText := str + stra;
 
@@ -183,6 +204,7 @@ end;
 
 procedure TfCompraRecebimento.BitBtn1Click(Sender: TObject);
 var TD: TTransactionDesc;
+  tudo, alteraStatus: String;
 begin
   TD.TransactionID := 1;
   TD.IsolationLevel := xilREADCOMMITTED;
@@ -191,6 +213,7 @@ begin
     cdsPedido.DisableControls;
     cdsPedido.First;
     DecimalSeparator := '.';
+    tudo := 'S';
     While not cdsPedido.Eof do
     begin
       if (cdsPedidoRECEBIDO.AsFloat > 0) then
@@ -198,10 +221,19 @@ begin
         dm.sqlsisAdimin.ExecuteDirect('UPDATE MOVIMENTODETALHE SET RECEBIDO = ' +
           FloatToStr(cdsPedidoRECEBIDO.asFloat) +
           ' WHERE CODDETALHE = ' + IntToStr(cdsPedidoCODDETALHE.AsInteger));
-      end;    
+      end;
+      if (cdsPedidoRECEBIDO.AsFloat < cdsPedidoQUANTIDADE.AsFloat) then
+        tudo := 'N';
       cdsPedido.Next;
     end;
     DecimalSeparator := ',';
+    if (tudo = 'S') then
+    begin
+      alteraStatus := 'UPDATE MOVIMENTO SET STATUS = 5 WHERE CODMOVIMENTO = ' + IntToStr(cdsPedidoCODMOVIMENTO.AsInteger);
+    end else begin
+      alteraStatus := 'UPDATE MOVIMENTO SET STATUS = 4 WHERE CODMOVIMENTO = ' + IntToStr(cdsPedidoCODMOVIMENTO.AsInteger);
+    end;
+    dm.sqlsisAdimin.ExecuteDirect(alteraStatus);
     dm.sqlsisAdimin.Commit(TD);
     cdsPedido.EnableControls;
     MessageDlg('Recebimento Gravado com sucesso.', mtInformation, [mbOK], 0);
@@ -211,7 +243,27 @@ begin
     DecimalSeparator := ',';
     exit;
   end;
+  bitbtn2.Click;
+end;
 
+procedure TfCompraRecebimento.btnClienteProcuraClick(Sender: TObject);
+begin
+  inherited;
+  fProcurar:= TfProcurar.Create(self,dm.scds_forn_proc);
+  dm.scds_forn_proc.Params.ParamByName('pStatus').AsInteger := 1;
+  dm.scds_forn_proc.Params.ParamByName('pSegmento').AsInteger := 0;
+  fProcurar.RadioGroup2.Visible := True;
+  fProcurar.BtnProcurar.Click;
+  fProcurar.EvDBFind1.DataField := 'NOMEFORNECEDOR';
+  fProcurar.btnIncluir.Visible := True;
+  try
+    fProcurar.ShowModal;
+    edFornec.Text     := IntToStr(dm.scds_forn_procCODFORNECEDOR.AsInteger);
+    edFornecNome.Text := dm.scds_forn_procNOMEFORNECEDOR.AsString;
+  finally
+    dm.scds_forn_proc.Close;
+    fProcurar.Free;
+  end;
 end;
 
 end.
