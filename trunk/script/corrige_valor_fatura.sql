a35e6ea51af1b01210a6c7b743fcc2bb0c93a817
douglas.darte <douglas.darte@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-06-10 19:03:51 +0000

    correção para não somar o ST qnd alterar apenas a Data

--- trunk/script/corrige_valor_fatura.sql
+++ trunk/script/corrige_valor_fatura.sql
@@ -3,9 +3,11 @@ BEFORE UPDATE POSITION 0
 AS 
 BEGIN 
     if ((old.VALOR_RESTO_SST is null) and (new.STATUS = '5-') )then
-        new.VALOR_RESTO_SST = old.VALOR_RESTO;
+        if(new.VALOR_RESTO <> old.VALOR_RESTO) then
+            new.VALOR_RESTO_SST = old.VALOR_RESTO;
     if ((old.VALOR_RESTO_SST is null) and (new.STATUS = '7-') )then
-        new.VALOR_RESTO_SST = old.VALORRECEBIDO;
+        if(new.VALOR_RESTO <> old.VALOR_RESTO) then
+            new.VALOR_RESTO_SST = old.VALORRECEBIDO;
     if (new.VALST >= 0) then
         new.VALOR_RESTO = (new.VALST + new.VALOR_RESTO_SST);
 END
