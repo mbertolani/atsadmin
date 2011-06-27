52b49ee4bba7217c70c9a928f5f0ce6d43162f27
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-06-27 14:25:20 +0000

    Desenvolvimento.

--- trunk/teste/prjTeste.dpr
+++ trunk/teste/prjTeste.dpr
@@ -1,16 +1,17 @@
 program prjTeste;
 
 uses
+  Forms,
   TestFrameWork,
   GuiTestRunner,
+  UDm in '..\comercial\src\UDm.pas' {DM: TDataModule},
   uEstoqueTeste in 'uEstoqueTeste.pas',
-  uEstoque in '..\comercial\src\uEstoque.pas',
-  UDm in '..\comercial\src\UDm.pas' {DM: TDataModule};
-
+  uEstoque in '..\comercial\src\uEstoque.pas';
 {$R *.res}
 
 begin
-  //Application.Initialize;
+  Application.Initialize;
+  Application.CreateForm(Tdm, dm);
   GUITestRunner.RunRegisteredTests;
 end.
 
