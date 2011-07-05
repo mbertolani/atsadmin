85460beb1502c977c619429174cf975b0da02e38
douglas.darte <douglas.darte@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-07-05 12:30:05 +0000

    inserção com CCusto Padrão

--- trunk/script/gera_pedido.sql
+++ trunk/script/gera_pedido.sql
@@ -22,14 +22,17 @@ BEGIN
         if (codmov is null) then 
           codMov = 0;
              
+        select p.DADOS from PARAMETRO p where p.PARAMETRO = 'CENTRO RECEITA PADRAO'
+        into :ccusto;
+             
         if (codmov = 0) then 
         begin  
           codmov = GEN_ID(GENMOV, 1);
           insert into MOVIMENTO (codmovimento, datamovimento, codcliente, codnatureza, 
-            status, codusuario, codfornecedor, data_sistema, controle, data_entrega, prazo_pagamento, obs)
+            status, codusuario, codfornecedor, data_sistema, controle, data_entrega, prazo_pagamento, obs, CODALMOXARIFADO)
           values (
             :codmov, CURRENT_DATE, 0, 5, 0, 1, new.COTACAO_FORNEC, CURRENT_TIMESTAMP, new.COTACAO_CODIGO,
-            new.COTACAO_DTENTREGA, new.COTACAO_PRAZO, UDF_LEFT(new.COTACAO_OBSERVACAO,99));   
+            new.COTACAO_DTENTREGA, new.COTACAO_PRAZO, UDF_LEFT(new.COTACAO_OBSERVACAO,99), :CCUSTO);
         end  
         select first 1 p.CODPRODUTO from produtos p where p.CODPRO =  new.COTACAO_ITEM
           into :codProduto;
