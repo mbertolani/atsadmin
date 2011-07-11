f6ea63392777df04373f763e195479a2be75ce55
douglas.darte <douglas.darte@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-07-11 14:16:13 +0000

    correções para o Inventario

--- trunk/comercial/src/uInventario.pas
+++ trunk/comercial/src/uInventario.pas
@@ -37,7 +37,6 @@ type
     cdsInventUN: TStringField;
     cdsProdCODPRO: TStringField;
     cdsProdCODPRODUTO: TIntegerField;
-    cdsProdPRODUTO: TStringField;
     cdsProdUNIDADEMEDIDA: TStringField;
     rgLista: TRadioGroup;
     cdsLanca_Inv: TClientDataSet;
@@ -64,6 +63,15 @@ type
     BitBtn2: TBitBtn;
     btnImprimir: TBitBtn;
     VCLReport1: TVCLReport;
+    cdsProdPRODUTO: TStringField;
+    cdsInventPRODUTO: TStringField;
+    JvDBGrid3: TJvDBGrid;
+    sdsListaInventario: TSQLDataSet;
+    dspListaInventario: TDataSetProvider;
+    cdsListaInventario: TClientDataSet;
+    dsListaInventario: TDataSource;
+    cdsListaInventarioCODIVENTARIO: TStringField;
+    cdsListaInventarioDATAIVENTARIO: TDateField;
     procedure btnProcClick(Sender: TObject);
     procedure btnProcListaClick(Sender: TObject);
     procedure JvDBGrid1CellClick(Column: TColumn);
@@ -77,6 +85,8 @@ type
     procedure cdsInventAfterPost(DataSet: TDataSet);
     procedure BitBtn2Click(Sender: TObject);
     procedure btnImprimirClick(Sender: TObject);
+    procedure JvDBGrid2TitleClick(Column: TColumn);
+    procedure JvDBGrid3CellClick(Column: TColumn);
   private
     { Private declarations }
     procedure incluirInventario;
@@ -89,7 +99,7 @@ var
 
 implementation
 
-uses UDm;
+uses UDm, dbXpress;
 
 {$R *.dfm}
 
@@ -126,43 +136,87 @@ procedure TfInventario.btnProcListaClick(Sender: TObject);
 var sqlb : string;
 begin
   sqlb := '';
-  if (cdsInvent.Active) then
-    cdsInvent.Close;
 
-  if (rgLista.ItemIndex = 0) then
+  if( (edLista.Text <> '') and (Dta.Text <> '  /  /    ')) then
   begin
-    sqlb := ' WHERE SITUACAO = ' + QuotedStr('A');
-  end;
-  if (rgLista.ItemIndex = 1) then
-  begin
-    sqlb := ' WHERE SITUACAO = ' + QuotedStr('G');
-  end;
+    if (cdsInvent.Active) then
+      cdsInvent.Close;
 
-  if (edLista.Text <> '') then
-  begin
-    sqlb := sqlb + ' AND CODIVENTARIO LIKE ' + QuotedStr(edLista.Text + '%');
-  end;
-
-  if (Dta.Text = '  /  /    ') then
-    sqlb := sqlb 
-  else
-    sqlb := sqlb + ' AND DATAIVENTARIO = ' + QuotedStr(formatdatetime('mm/dd/yy', dta.Date));
-
-  cdsInvent.CommandText := 'SELECT * FROM INVENTARIO ' + sqlb;
-  cdsInvent.Open;
-  if (cdsInvent.IsEmpty) then
-  begin
-    if MessageDlg('Não existe esta Lista, criar uma ?',mtConfirmation,
-                [mbYes,mbNo],0) = mrYes then
+    if (rgLista.ItemIndex = 0) then
     begin
-      cdsInvent.Append;
-      cdsInventCODIVENTARIO.AsString := edLista.text;
+      sqlb := ' WHERE i.SITUACAO = ' + QuotedStr('A');
     end;
-  end;
+    if (rgLista.ItemIndex = 1) then
+    begin
+      sqlb := ' WHERE i.SITUACAO = ' + QuotedStr('G');
+    end;
 
+    if (edLista.Text <> '') then
+    begin
+      sqlb := sqlb + ' AND i.CODIVENTARIO LIKE ' + QuotedStr(edLista.Text + '%');
+    end;
+
+    if (Dta.Text = '  /  /    ') then
+      sqlb := sqlb
+    else
+      sqlb := sqlb + ' AND i.DATAIVENTARIO = ' + QuotedStr(formatdatetime('mm/dd/yy', dta.Date));
+
+    cdsInvent.CommandText := 'SELECT i.*, cast(p.produto as varchar(300)) produto FROM INVENTARIO i ' +
+    'inner join produtos p on p.codproduto = i.codproduto ' + sqlb;
+    cdsInvent.Open;
+    if (cdsInvent.IsEmpty) then
+    begin
+      if MessageDlg('Não existe esta Lista, criar uma ?',mtConfirmation,
+                  [mbYes,mbNo],0) = mrYes then
+      begin
+        cdsInvent.Append;
+        cdsInventCODIVENTARIO.AsString := edLista.text;
+      end;
+    end;
+  end
+  else
+  begin
+    if (cdsListaInventario.Active) then
+      cdsListaInventario.Close;
+
+    if (rgLista.ItemIndex = 0) then
+    begin
+      sqlb := ' WHERE i.SITUACAO = ' + QuotedStr('A');
+    end;
+    if (rgLista.ItemIndex = 1) then
+    begin
+      sqlb := ' WHERE i.SITUACAO = ' + QuotedStr('G');
+    end;
+
+    if (edLista.Text <> '') then
+    begin
+      sqlb := sqlb + ' AND i.CODIVENTARIO LIKE ' + QuotedStr(edLista.Text + '%');
+    end;
+
+    if (Dta.Text = '  /  /    ') then
+      sqlb := sqlb
+    else
+      sqlb := sqlb + ' AND i.DATAIVENTARIO = ' + QuotedStr(formatdatetime('mm/dd/yy', dta.Date));
+
+    cdsListaInventario.CommandText := 'SELECT i.*FROM INVENTARIO i ' + sqlb;
+    cdsListaInventario.Open;
+    if (cdsListaInventario.IsEmpty) then
+    begin
+      if MessageDlg('Não existe esta Lista, criar uma ?',mtConfirmation,
+                  [mbYes,mbNo],0) = mrYes then
+      begin
+        cdsInvent.Append;
+        cdsInventCODIVENTARIO.AsString := edLista.text;
+      end;
+    end;
+
+
+  end;
 end;
 
 procedure TfInventario.incluirInventario;
+var sql :string;
+    TD : TTransactionDesc;
 begin
   if (edLista.Text = '') then
   begin
@@ -174,14 +228,26 @@ begin
   begin
     if (cdsInvent.State in [dsBrowse]) then
     begin
-      cdsInvent.Append;
+        sql := 'INSERT INTO INVENTARIO (CODIVENTARIO, DATAIVENTARIO, CODPRODUTO, CODPRO, SITUACAO, UN) VALUES ('  +
+      QuotedStr(edLista.text) + ' , ' + QuotedStr(formatdatetime('mm/dd/yyyy', Now)) + ', ' + IntToStr(cdsProd.Fields[1].AsInteger) + ', ' +
+      QuotedStr(cdsProd.Fields[0].AsString) + ', ' +QuotedStr('A') + ', ' + QuotedStr(cdsProd.Fields[3].AsString) + ')';
+    try
+    TD.TransactionID := 1;
+    TD.IsolationLevel := xilREADCOMMITTED;
+    dm.sqlsisAdimin.StartTransaction(TD);
+    dm.sqlsisAdimin.ExecuteDirect(sql);
+    dm.sqlsisAdimin.Commit(TD);
+    except
+     MessageDlg('Erro ao inserir no banco de dados', mtError, [mbOK], 0);
+    end;
+      {cdsInvent.Append;
       cdsInventCODIVENTARIO.AsString    := edLista.text;
       cdsInventDATAIVENTARIO.AsDateTime := now;
       cdsInventCODPRODUTO.AsInteger     := cdsProd.Fields[1].AsInteger;
       cdsInventCODPRO.AsString          := cdsProd.Fields[0].AsString;
       cdsInventSITUACAO.AsString        := 'A';
       cdsInventUN.AsString              := cdsProd.Fields[3].AsString;
-      cdsInvent.ApplyUpdates(0);
+      cdsInvent.ApplyUpdates(0);}
     end;
     if (cdsInvent.State in [dsInsert]) then
     begin
@@ -263,12 +329,14 @@ begin
     cdsProd.Next;
   end;
   cdsProd.EnableControls;
+  btnProcLista.Click;
 end;
 
 procedure TfInventario.btnIncluiClick(Sender: TObject);
 begin
   //inherited;
   incluirInventario;
+  btnProcLista.Click;
 end;
 
 procedure TfInventario.btnRemoveTodosClick(Sender: TObject);
@@ -287,7 +355,7 @@ begin
   if (cdsInvent.Active) then
   begin
     cdsInvent.Delete;
-  end;  
+  end;
 end;
 
 procedure TfInventario.FormCreate(Sender: TObject);
@@ -318,6 +386,66 @@ begin
     VCLReport1.Report.Params.ParamByName('CODIVENTARIO').Value := cdsInventCODIVENTARIO.AsString;
     VCLReport1.Report.Params.ParamByName('DATAIVENTARIO').Value :=  formatdatetime('dd/mm/yy', StrToDate(cdsInventDATAIVENTARIO.AsString));
     VCLReport1.Execute;
+end;
+
+procedure TfInventario.JvDBGrid2TitleClick(Column: TColumn);
+var
+  enum_IndexOption: TIndexOptions;
+  str_IndexAsc,
+  str_IndexDesc,
+  str_IndexName: String;
+begin
+  if (Column.Field.FieldKind = fkData) then
+  begin
+    str_IndexAsc := Concat('asc_',Column.FieldName);
+    str_IndexDesc := Concat('desc_',Column.FieldName);
+
+    str_IndexName := '';
+    enum_IndexOption := [];
+
+    if (cdsInvent.IndexName = str_IndexAsc) then
+    begin
+    str_IndexName := str_IndexDesc;
+    enum_IndexOption := [ixDescending];
+    end
+    else if (cdsInvent.IndexName = str_IndexDesc) then
+    begin
+    str_IndexName := str_IndexAsc;
+    end
+    else
+    begin
+    str_IndexName := str_IndexAsc;
+    end;
+
+    cdsInvent.IndexDefs.Clear;
+
+    cdsInvent.IndexDefs.Add(str_IndexName,Column.FieldName,enum_IndexOption);
+    cdsInvent.IndexName := str_IndexName;
+  end;
+end;
+
+procedure TfInventario.JvDBGrid3CellClick(Column: TColumn);
+var sqlb : String;
+begin
+  sqlb := '';
+  if (cdsInvent.Active) then
+    cdsInvent.Close;
+
+  if (rgLista.ItemIndex = 0) then
+  begin
+    sqlb := ' WHERE i.SITUACAO = ' + QuotedStr('A');
+  end;
+  if (rgLista.ItemIndex = 1) then
+  begin
+    sqlb := ' WHERE i.SITUACAO = ' + QuotedStr('G');
+  end;
+
+  sqlb := sqlb + ' AND i.CODIVENTARIO = ' + QuotedStr(cdsListaInventarioCODIVENTARIO.AsString);
+  sqlb := sqlb + ' AND i.DATAIVENTARIO = ' + QuotedStr(formatdatetime('mm/dd/yy', cdsListaInventarioDATAIVENTARIO.AsDateTime));
+
+  cdsInvent.CommandText := 'SELECT i.*, cast(p.produto as varchar(300)) produto FROM INVENTARIO i ' +
+  'inner join produtos p on p.codproduto = i.codproduto ' + sqlb;
+  cdsInvent.Open;
 end;
 
 end.
