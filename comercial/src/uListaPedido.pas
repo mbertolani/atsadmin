00b25aaa6dc72c7c698cfec746877227e3e37f77
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-08-09 19:42:15 +0000

    Corrigido para mostrar os pedidos em aberto, e so permite adicionar itens se o pedido estiver com Status = 0. E Agora os itens devem ser selecionados para gerar o pedido.

--- comercial/src/uListaPedido.pas
+++ comercial/src/uListaPedido.pas
@@ -4,12 +4,20 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, ComCtrls, Grids, DBGrids, DB;
+  Dialogs, ComCtrls, Grids, DBGrids, DB, FMTBcd, DBClient, Provider,
+  SqlExpr;
 
 type
   TfListaPedido = class(TForm)
     DBGrid1: TDBGrid;
     dsPedido: TDataSource;
+    sqlPedido: TSQLQuery;
+    dspPedido: TDataSetProvider;
+    cdsPedido: TClientDataSet;
+    cdsPedidoCODMOVIMENTO: TIntegerField;
+    cdsPedidoSTATUS: TSmallintField;
+    cdsPedidoCODPEDIDO: TIntegerField;
+    cdsPedidoDATAMOVIMENTO: TDateField;
   private
     { Private declarations }
   public
@@ -20,6 +28,8 @@ var
   fListaPedido: TfListaPedido;
 
 implementation
+
+uses UDm;
 
 {$R *.dfm}
 
