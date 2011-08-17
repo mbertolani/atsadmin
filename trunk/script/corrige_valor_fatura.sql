bbc4f19d55e17801dad785fddfb895fc92fa1da9
douglas.darte <douglas.darte@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-08-17 19:58:46 +0000

    correção para carregar ST

--- trunk/script/corrige_valor_fatura.sql
+++ trunk/script/corrige_valor_fatura.sql
@@ -2,13 +2,18 @@ CREATE OR ALTER TRIGGER CORRIGE_VALOR_FATURA FOR RECEBIMENTO ACTIVE
 BEFORE UPDATE POSITION 0
 AS 
 BEGIN 
-    if ((old.VALOR_RESTO_SST is null) and (new.STATUS = '5-') )then
-        new.VALOR_RESTO_SST = old.VALOR_RESTO;
-    if(new.VALOR_RESTO <> old.VALOR_RESTO) then
-        new.VALOR_RESTO_SST = new.VALOR_RESTO;
-    if ((old.VALOR_RESTO_SST is null) and (new.STATUS = '7-') )then
-        if(new.VALOR_RESTO <> old.VALOR_RESTO) then
+    if (old.VALOR_RESTO_SST is null)then
+    begin
+        if (new.STATUS = '5-') then
+            new.VALOR_RESTO_SST = old.VALOR_RESTO;
+        if (new.STATUS = '7-') then
             new.VALOR_RESTO_SST = old.VALORRECEBIDO;
-    if (new.VALST >= 0) then
+    end
+    if(new.VALST is null) then
+        new.VALST = 0;
+    if ((new.VALST <> old.VALST) and (new.STATUS = '5-') ) then
         new.VALOR_RESTO = (new.VALST + new.VALOR_RESTO_SST);
+    if ((new.VALST = 0) and (new.STATUS = '5-') ) then
+        new.VALOR_RESTO = new.VALOR_RESTO_SST;
+
 END
