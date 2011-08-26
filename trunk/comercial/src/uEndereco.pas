b64567994c66fac96d769d00edf0608f8fc782c7
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-08-26 00:35:58 +0000

    Desenvolvimento.

---
+++ trunk/comercial/src/uEndereco.pas
@@ -0,0 +1,44 @@
+unit uEndereco;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, uPai_new, ExtCtrls, Mask, JvExMask, JvToolEdit, JvMaskEdit,
+  StdCtrls, Menus, XPMenu, DB, Buttons, MMJPanel;
+
+type
+  TfEndereco = class(TfPai_new)
+    GroupBox4: TGroupBox;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    edLogradouro: TEdit;
+    edNumero: TEdit;
+    edBairro: TEdit;
+    edComplemento: TEdit;
+    BitBtn1: TBitBtn;
+    edCidade: TEdit;
+    edUF: TEdit;
+    edCEP: TJvMaskEdit;
+    edIBGE: TEdit;
+    RadioGroup2: TRadioGroup;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  fEndereco: TfEndereco;
+
+implementation
+
+{$R *.dfm}
+
+end.
