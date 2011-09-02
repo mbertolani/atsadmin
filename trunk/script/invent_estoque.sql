754b0380cf0f67208d8d7f9ac6d36cef7eb52b02
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-09-02 14:11:07 +0000

    Correção de erro.

--- trunk/script/invent_estoque.sql
+++ trunk/script/invent_estoque.sql
@@ -1,28 +1,36 @@
 set term  ^ ;
+
 CREATE OR ALTER TRIGGER INVENT_ESTOQUE FOR INVENTARIO
 ACTIVE BEFORE INSERT POSITION 0
 AS 
 declare variable qtde double precision;
+declare variable mesAno Date;
+declare variable ccusto integer;
 BEGIN 
   if (inserting) then 
   begin
+    ccusto = new.CODCCUSTO;
+    if (ccusto is null) then 
+      ccusto = 0;
     qtde = 0; 
 	-- Preenche o campo ESTOQUE_ATUAL
     -- Ve o estoque atual
-    IF (new.CODCCUSTO = 0) then 
+    IF (ccusto = 0) then 
     begin 
-      SELECT First 1 m.SALDOESTOQUE FROM ESTOQUEMES M 
-       WHERE m.MESANO = UDF_MONTHSTART(UDF_YEAR(NEW.DATAIVENTARIO), UDF_MONTH(NEW.DATAIVENTARIO)) 
+      SELECT First 1 m.SALDOESTOQUE, m.MESANO FROM ESTOQUEMES M 
+       WHERE m.MESANO <= UDF_MONTHSTART(UDF_YEAR(NEW.DATAIVENTARIO), UDF_MONTH(NEW.DATAIVENTARIO)) 
          AND m.CODPRODUTO = NEW.CODPRODUTO
-         into :qtde;   
+       ORDER BY 2 DESC 
+         into :qtde, :mesAno;   
     end      
-    IF (new.CODCCUSTO > 0) then 
+    IF (ccusto > 0) then 
     begin 
-      SELECT First 1 m.SALDOESTOQUE FROM ESTOQUEMES M 
-       WHERE m.MESANO = UDF_MONTHSTART(UDF_YEAR(NEW.DATAIVENTARIO), UDF_MONTH(NEW.DATAIVENTARIO)) 
+      SELECT First 1 m.SALDOESTOQUE, m.MESANO FROM ESTOQUEMES M 
+       WHERE m.MESANO <= UDF_MONTHSTART(UDF_YEAR(NEW.DATAIVENTARIO), UDF_MONTH(NEW.DATAIVENTARIO)) 
          AND m.CODPRODUTO = NEW.CODPRODUTO
          AND m.CENTROCUSTO = NEW.CODCCUSTO
-         into :qtde;   
+       ORDER BY 2 DESC 
+         into :qtde, :mesAno;   
     end      
 
     if (qtde is null) then 
