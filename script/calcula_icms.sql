261380934bbca6727c6e6e9e307f8ff8e02fd7c7
douglas.darte <douglas.darte@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-10-14 15:58:15 +0000

    alterações para carregar valor total nota

--- script/calcula_icms.sql
+++ script/calcula_icms.sql
@@ -30,7 +30,11 @@ declare variable VFRETE DOUBLE PRECISION;
 declare variable VOUTROS DOUBLE PRECISION;
 declare variable bcfrete DOUBLE PRECISION;
 declare variable stfrete DOUBLE PRECISION;
+declare variable bcstfrete DOUBLE PRECISION;
+declare variable icmsfrete DOUBLE PRECISION;
 declare variable DESCONTO DOUBLE PRECISION;
+declare variable VSEGURO DOUBLE PRECISION;
+declare variable OUTRAS DOUBLE PRECISION;
 declare variable codCli integer;
 declare variable d9 SMALLINT;
 declare variable nat SMALLINT;
@@ -77,8 +81,8 @@ begin
       ICMS_DESTACADO_DESC = 'Base Calculo ICMS = ' || :VlrStr;
       ICMS_DESTACADO_DESC2 = 'Aliquota = ' || cast(:icms_destacado as varchar(5)) || '% - Icms = ' || :PercStr;
     end
+	UPDATE NOTAFISCAL SET  CORPONF5 = :ICMS_DESTACADO_DESC, CORPONF6 = :ICMS_DESTACADO_DESC2 where NUMNF = :NUMERO_NF;	
   end
-  UPDATE NOTAFISCAL SET  CORPONF5 = :ICMS_DESTACADO_DESC, CORPONF6 = :ICMS_DESTACADO_DESC2 where NUMNF = :NUMERO_NF;
   /* ==============================================================*/
 
     select v.codmovimento, v.NOTAFISCAL, v.SERIE, v.CODVENDA, v.prazo, n.NATUREZA, n.VALOR_FRETE, n.OUTRAS_DESP, n.VALOR_SEGURO, n.VALOR_DESCONTO from venda v
@@ -86,20 +90,20 @@ begin
     into :cod, :notaFiscalVenda, :serie, :codv, :prazo, :nat, :VFRETE, :SEG, :VOUTROS, :DESCONTO; 
     	
     if (nat is null) then
-    select c.codmovimento, c.NOTAFISCAL, c.SERIE, c.CODCOMPRA, c.prazo, n.NATUREZA, n.VALOR_DESCONTO from COMPRA c
+    select c.codmovimento, c.NOTAFISCAL, c.SERIE, c.CODCOMPRA, c.prazo, n.NATUREZA, n.VALOR_FRETE, n.OUTRAS_DESP, n.VALOR_SEGURO, n.VALOR_DESCONTO from COMPRA c
         inner join notafiscal n on n.CODVENDA = c.CODCOMPRA where n.NUMNF = :numero_nf and (n.NATUREZA = 20 or n.NATUREZA = 21)
-    into :cod, :notaFiscalVenda, :serie, :codv, :prazo, :nat, :DESCONTO;              
+    into :cod, :notaFiscalVenda, :serie, :codv, :prazo, :nat, :VFRETE, :SEG, :VOUTROS, :DESCONTO;              
 
-    select COALESCE(sum(md.ICMS_SUBST), 0), COALESCE(sum(md.ICMS_SUBSTD), 0), COALESCE(sum(md.VALOR_ICMS), 0), COALESCE(sum(md.VLR_BASEICMS), 0), COALESCE(sum(md.VIPI), 0), COALESCE(sum(md.QUANTIDADE * md.VLR_BASE), 0) from MOVIMENTODETALHE md
+    select COALESCE(sum(md.ICMS_SUBST), 0), COALESCE(sum(md.ICMS_SUBSTD), 0), COALESCE(sum(md.VALOR_ICMS), 0), COALESCE(sum(md.VLR_BASEICMS), 0)
+    , COALESCE(sum(md.VIPI), 0), COALESCE(sum(md.QUANTIDADE * md.VLR_BASE), 0) , COALESCE(sum(md.VALOR_DESCONTO), 0), COALESCE(sum(md.VALOR_SEGURO), 0)
+    , COALESCE(sum(md.VALOR_OUTROS), 0) , COALESCE(sum(md.FRETE), 0) 
+    from MOVIMENTODETALHE md
     where md.CODMOVIMENTO = :cod
-    into :TOTST, :TOTBASEST, :TOTICMS, :TOTBASEICMS, :TOTIPI, :TOTPROD;
+    into :TOTST, :TOTBASEST, :TOTICMS, :TOTBASEICMS, :TOTIPI, :TOTPROD, :DESCONTO, :VSEGURO, :OUTRAS, :VFRETE;
     
-    select first 1 COALESCE(md.BCFRETE, 0), COALESCE(md.STFRETE, 0) from MOVIMENTODETALHE md
-    where md.CODMOVIMENTO = :cod
-    into :bcfrete, :stfrete;
-    
-    UPDATE NOTAFISCAL SET BASE_ICMS_SUBST = :TOTBASEST + :BCFRETE, VALOR_ICMS_SUBST = :TOTST + :STFRETE, VALOR_IPI = :TOTIPI, VALOR_ICMS = :TOTICMS, BASE_ICMS = :TOTBASEICMS ,
-      VALOR_TOTAL_NOTA = :TOTPROD + :TOTST + :TOTIPI + :VFRETE + :SEG + :VOUTROS + :STFRETE - :DESCONTO
+
+    UPDATE NOTAFISCAL SET BASE_ICMS_SUBST = :TOTBASEST , VALOR_ICMS_SUBST = :TOTST , VALOR_IPI = :TOTIPI, VALOR_ICMS = :TOTICMS , BASE_ICMS = :TOTBASEICMS ,
+      VALOR_TOTAL_NOTA = :TOTPROD + :TOTST + :TOTIPI + :VSEGURO - :DESCONTO + :OUTRAS  + :VFRETE  
       where NUMNF = :numero_nf;
               
   
@@ -107,7 +111,7 @@ begin
       into :numerofatura;        
       NumeroFatura = notaFiscalVenda || '-' || serie;
         
-    -- vejo se usa nf Parcial
+    /* vejo se usa nf Parcial */
     select parametro from parametro where parametro = 'NFVALOR'
     into :parametro;
     if (parametro is not null) then
