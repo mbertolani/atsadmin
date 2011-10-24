5f6793e23cab648a2207d17186a22fd8816a886f
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-10-24 12:47:28 +0000

    Proc. Grafico Venda e Compra.

---
+++ trunk/script/pedido_x_venda.sql
@@ -0,0 +1,28 @@
+SET TERM ^ ;
+
+CREATE OR ALTER PROCEDURE pedido_x_venda 
+ ( dataIni date, dataFim date ) 
+RETURNS 
+ ( mesAno char(7), mesDesc char(3), valorVenda double precision, valorPedido double precision)
+AS 
+DECLARE VARIABLE  mes smallint;
+DECLARE VARIABLE  ano smallint; 
+BEGIN
+  /* Mostra o Total de Pedidos no Mes e o Total Faturado no Mes */
+  For SELECT UDF_MONTH(DATAVENDA), UDF_YEAR(DATAVENDA) , UDF_SMONTHNAME(DATAVENDA) MES, SUM(VALOR) VENDA
+    FROM VENDA 
+   WHERE DATAVENDA BETWEEN :dataIni AND :dataFim 
+   GROUP BY UDF_MONTH(DATAVENDA), UDF_YEAR(DATAVENDA), UDF_SMONTHNAME(DATAVENDA)
+    INTO :mes, :ano, :mesDesc, :valorVenda
+  do begin
+    mesAno = UDF_PADL(mes,'0',2) || '-' || cast(ano as char(4));
+    -- Total de Pedido no Período
+    Select sum(md.VLR_BASE * md.QUANTIDADE) from Movimento m , MovimentoDetalhe md 
+     where md.CODMOVIMENTO = m.CODMOVIMENTO 
+       and udf_month(m.DATAMOVIMENTO) = :mes and UDF_YEAR(m.DATAMOVIMENTO) = :ano
+    into :valorPedido;      
+    Suspend; 
+  end   
+END^
+
+SET TERM ; ^
