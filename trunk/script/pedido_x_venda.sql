064b966acfc17d11d8b1cd282ef481d5098748ed
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-10-24 12:56:59 +0000

    Proc. Grafico Venda e Compra.

--- trunk/script/pedido_x_venda.sql
+++ trunk/script/pedido_x_venda.sql
@@ -1,28 +1,55 @@
 SET TERM ^ ;
 
 CREATE OR ALTER PROCEDURE pedido_x_venda 
- ( dataIni date, dataFim date ) 
+ ( dataIni date, dataFim date, compraVenda char(1) ) 
 RETURNS 
- ( mesAno char(7), mesDesc char(3), valorVenda double precision, valorPedido double precision)
+ ( mesAno char(7), mesAnoV integer, valorVenda double precision, valorPedido double precision)
 AS 
 DECLARE VARIABLE  mes smallint;
 DECLARE VARIABLE  ano smallint; 
 BEGIN
-  /* Mostra o Total de Pedidos no Mes e o Total Faturado no Mes */
-  For SELECT UDF_MONTH(DATAVENDA), UDF_YEAR(DATAVENDA) , UDF_SMONTHNAME(DATAVENDA) MES, SUM(VALOR) VENDA
+  -- Mostra o Total de Pedidos no Mes e o Total Faturado no Mes 
+  if (compraVenda = 'V') then 
+  begin 
+  For SELECT UDF_MONTH(DATAVENDA), UDF_YEAR(DATAVENDA) , UDF_MONTH(DATAVENDA) || UDF_YEAR(DATAVENDA) MESANO, SUM(VALOR) VENDA
     FROM VENDA 
    WHERE DATAVENDA BETWEEN :dataIni AND :dataFim 
-   GROUP BY UDF_MONTH(DATAVENDA), UDF_YEAR(DATAVENDA), UDF_SMONTHNAME(DATAVENDA)
-    INTO :mes, :ano, :mesDesc, :valorVenda
+   GROUP BY UDF_MONTH(DATAVENDA), UDF_YEAR(DATAVENDA), UDF_MONTH(DATAVENDA)
+    INTO :mes, :ano, :mesAnoV, :valorVenda
   do begin
     mesAno = UDF_PADL(mes,'0',2) || '-' || cast(ano as char(4));
     -- Total de Pedido no Período
     Select sum(md.VLR_BASE * md.QUANTIDADE) from Movimento m , MovimentoDetalhe md 
      where md.CODMOVIMENTO = m.CODMOVIMENTO 
+       and m.CODNATUREZA   = 3
        and udf_month(m.DATAMOVIMENTO) = :mes and UDF_YEAR(m.DATAMOVIMENTO) = :ano
     into :valorPedido;      
     Suspend; 
-  end   
+  end  
+  mes = null;
+  ano = null;
+  end
+
+  -- Mostra o Total de Pedidos no Mes e o Total Recebido no Mes 
+  if (compraVenda = 'C') then 
+  begin 
+  For SELECT UDF_MONTH(DATACOMPRA), UDF_YEAR(DATACOMPRA) , UDF_MONTH(DATACOMPRA) || UDF_YEAR(DATACOMPRA) MESANO, SUM(VALOR) COMPRA
+    FROM COMPRA 
+   WHERE DATACOMPRA BETWEEN :dataIni AND :dataFim 
+   GROUP BY UDF_MONTH(DATACOMPRA), UDF_YEAR(DATACOMPRA), UDF_MONTH(DATACOMPRA)
+    INTO :mes, :ano, :mesAnoV, :valorVenda
+  do begin
+    mesAno = UDF_PADL(mes,'0',2) || '-' || cast(ano as char(4));
+    -- Total de Pedido no Período
+    Select sum(md.VLR_BASE * md.QUANTIDADE) from Movimento m , MovimentoDetalhe md 
+     where md.CODMOVIMENTO = m.CODMOVIMENTO 
+       and m.CODNATUREZA   = 4
+       and udf_month(m.DATAMOVIMENTO) = :mes and UDF_YEAR(m.DATAMOVIMENTO) = :ano
+    into :valorPedido;      
+    Suspend; 
+  end  
+  end
+   
 END^
 
 SET TERM ; ^
