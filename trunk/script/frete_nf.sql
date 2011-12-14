0a48c3bc4ec95a1a3ced5fa44e965e6f23942f97
douglas.darte <douglas.darte@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-12-14 18:22:01 +0000

    adição da BCII e II para distribuição igual nos campos

--- trunk/script/frete_nf.sql
+++ trunk/script/frete_nf.sql
@@ -9,17 +9,23 @@ declare variable FRETE double precision;
 declare variable DESCONTO double precision;
 declare variable OUTRAS double precision;
 declare variable SEGURO double precision;
+declare variable BCII double precision;
+declare variable II double precision;
 declare variable FRETE_UNIT double precision;
 declare variable DESCONTO_UNIT double precision;
 declare variable OUTRAS_UNIT double precision;
 declare variable SEGURO_UNIT double precision;
+declare variable BCII_UNIT  double precision;
+declare variable II_UNIT  double precision;
 declare variable FRETE_TOTAL double precision;
 declare variable DESCONTO_TOTAL double precision;
 declare variable OUTRAS_TOTAL double precision;
 declare variable SEGURO_TOTAL double precision;
+declare variable BCII_TOTAL double precision;
+declare variable II_TOTAL double precision;
 declare variable valtot double precision;
 BEGIN
---alter table MOVIMENTODETALHE add VALOR_DESCONTO double precision
+
     if ((new.NATUREZA = 15) or (new.NATUREZA = 12) or (new.NATUREZA = 16)) then
     begin
         select v.CODMOVIMENTO from venda v
@@ -37,6 +43,8 @@ BEGIN
    DESCONTO = new.VALOR_DESCONTO;
    OUTRAS = new.OUTRAS_DESP;
    SEGURO = new.VALOR_SEGURO;
+   II = new.II;
+   BCII = new.BCII;
    if (frete is null) then
     frete = 0;
    if (desconto is null) then
@@ -45,6 +53,10 @@ BEGIN
     OUTRAS = 0; 
    if (SEGURO is null) then
     SEGURO = 0; 
+   if (II is null) then
+    II = 0; 
+   if (BCII is null) then
+    BCII = 0;     
    
    if ( new.VALOR_PRODUTO > 0) then
    begin
@@ -52,12 +64,16 @@ BEGIN
      DESCONTO_UNIT = new.VALOR_DESCONTO / new.VALOR_PRODUTO;
      OUTRAS_UNIT = new.OUTRAS_DESP / new.VALOR_PRODUTO;
      SEGURO_UNIT = new.VALOR_SEGURO / new.VALOR_PRODUTO;
+     II_UNIT = new.II / new.VALOR_PRODUTO;
+     BCII_UNIT = new.BCII / new.VALOR_PRODUTO;
    end
    
    Frete_TOTAL = new.VALOR_FRETE;
    DESCONTO_TOTAL = new.VALOR_DESCONTO;
    OUTRAS_TOTAL = new.OUTRAS_DESP;
    SEGURO_TOTAL = new.VALOR_SEGURO;
+   II_TOTAL = new.II;
+   BCII_TOTAL = new.BCII;
    
    select count(md.CODDETALHE) from MOVIMENTODETALHE md
    where md.CODMOVIMENTO = :codm
@@ -71,16 +87,24 @@ BEGIN
 		valtot = 1;
 	if (inserting) then
 	begin
-	 update MOVIMENTODETALHE set FRETE = UDF_ROUNDDEC((:Frete_UNIT * :valtot), 2), valor_desconto = UDF_ROUNDDEC((:DESCONTO_UNIT * :valtot), 2), valor_outros = UDF_ROUNDDEC((:OUTRAS_UNIT * :valtot), 2), valor_SEGURO = UDF_ROUNDDEC((:SEGURO_UNIT * :valtot), 2) where CODDETALHE = :coddet;
+	 update MOVIMENTODETALHE set FRETE = UDF_ROUNDDEC((:Frete_UNIT * :valtot), 2), valor_desconto = UDF_ROUNDDEC((:DESCONTO_UNIT * :valtot), 2), 
+	 valor_outros = UDF_ROUNDDEC((:OUTRAS_UNIT * :valtot), 2), valor_SEGURO = UDF_ROUNDDEC((:SEGURO_UNIT * :valtot), 2),
+	 BCII = UDF_ROUNDDEC((:BCII_UNIT * :valtot), 2), II = UDF_ROUNDDEC((:II_UNIT * :valtot), 2) where CODDETALHE = :coddet;
 	 if ( det = det1) then
-	   update MOVIMENTODETALHE set FRETE = :Frete_TOTAL, valor_desconto = :DESCONTO_TOTAL, valor_seguro = :SEGURO_TOTAL, valor_outros = :OUTRAS_TOTAL where CODDETALHE = :coddet;
+	   update MOVIMENTODETALHE set FRETE = :Frete_TOTAL, valor_desconto = :DESCONTO_TOTAL, valor_seguro = :SEGURO_TOTAL, 
+	   valor_outros = :OUTRAS_TOTAL, BCII = :BCII_TOTAL, II = :II_TOTAL where CODDETALHE = :coddet;
     end
 	else
      if((old.VALOR_FRETE <> new.VALOR_FRETE) or (old.VALOR_DESCONTO <> new.VALOR_DESCONTO) or (old.VALOR_SEGURO <> new.VALOR_SEGURO) or (old.OUTRAS_DESP <> new.OUTRAS_DESP)) then
      begin
-        update MOVIMENTODETALHE set FRETE = UDF_ROUNDDEC((:Frete_UNIT * :valtot), 2), valor_desconto = UDF_ROUNDDEC((:DESCONTO_UNIT * :valtot), 2), valor_outros = UDF_ROUNDDEC((:OUTRAS_UNIT * :valtot), 2), valor_SEGURO = UDF_ROUNDDEC((:SEGURO_UNIT * :valtot), 2) where CODDETALHE = :coddet;
+        update MOVIMENTODETALHE set FRETE = UDF_ROUNDDEC((:Frete_UNIT * :valtot), 2), valor_desconto = UDF_ROUNDDEC((:DESCONTO_UNIT * :valtot), 2), 
+        valor_outros = UDF_ROUNDDEC((:OUTRAS_UNIT * :valtot), 2), valor_SEGURO = UDF_ROUNDDEC((:SEGURO_UNIT * :valtot), 2), 
+        BCII = UDF_ROUNDDEC((:BCII_UNIT * :valtot), 2), II = UDF_ROUNDDEC((:II_UNIT * :valtot), 2)
+        where CODDETALHE = :coddet;
         if ( det = det1) then
-	      update MOVIMENTODETALHE set FRETE = :Frete_TOTAL, valor_desconto = :DESCONTO_TOTAL, valor_seguro = :SEGURO_TOTAL, valor_outros = :OUTRAS_TOTAL where CODDETALHE = :coddet;
+	      update MOVIMENTODETALHE set FRETE = :Frete_TOTAL, valor_desconto = :DESCONTO_TOTAL, valor_seguro = :SEGURO_TOTAL, 
+	      valor_outros = :OUTRAS_TOTAL, BCII = :BCII_TOTAL, II = :II_TOTAL
+	      where CODDETALHE = :coddet;
     end
     det1 = det1 +1;
     Frete_TOTAL   = Frete_TOTAL - UDF_ROUNDDEC((:Frete_UNIT * :valtot), 2);
