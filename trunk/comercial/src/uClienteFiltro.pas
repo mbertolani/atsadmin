7b2c6ef95a6aa3d1f2b1a5b8c5b31c99462e7a59
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2011-12-18 23:57:19 +0000

    Adicionado Filtro para ListaCliente e uma Classe Receber.

--- trunk/comercial/src/uClienteFiltro.pas
+++ trunk/comercial/src/uClienteFiltro.pas
@@ -5,38 +5,33 @@ interface
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, Mask, JvExMask, JvToolEdit, JvMaskEdit, JvCheckedMaskEdit,
-  JvDatePickerEdit, StdCtrls, Buttons;
+  JvDatePickerEdit, StdCtrls, Buttons, ExtCtrls;
 
 type
   TfClienteFiltro = class(TForm)
-    GroupBox6: TGroupBox;
-    CBox3: TRadioButton;
-    CBox4: TRadioButton;
-    cbtemail: TRadioButton;
-    GroupBox1: TGroupBox;
-    CBox1: TRadioButton;
-    CBox2: TRadioButton;
     GroupBox2: TGroupBox;
     Label1: TLabel;
     Label2: TLabel;
     codVen: TEdit;
     nomeVen: TEdit;
     BitBtn5: TBitBtn;
-    GroupBox5: TGroupBox;
-    Label5: TLabel;
-    Label7: TLabel;
-    edtUF: TEdit;
-    edtDDD: TEdit;
+    Panel1: TPanel;
+    BitBtn3: TBitBtn;
+    BitBtn2: TBitBtn;
+    BitBtn13: TBitBtn;
+    Panel2: TPanel;
     GroupBox7: TGroupBox;
     cbfisica: TRadioButton;
     cbjuridica: TRadioButton;
     cbttipo: TRadioButton;
-    Label10: TLabel;
-    JvDatePickerEdit2: TJvDatePickerEdit;
-    JvDatePickerEdit3: TJvDatePickerEdit;
-    GroupBox4: TGroupBox;
-    edCidade: TEdit;
-    BitBtn1: TBitBtn;
+    GroupBox1: TGroupBox;
+    CBox1: TRadioButton;
+    CBox2: TRadioButton;
+    GroupBox6: TGroupBox;
+    CBox3: TRadioButton;
+    CBox4: TRadioButton;
+    cbtemail: TRadioButton;
+    Panel3: TPanel;
     GroupBox3: TGroupBox;
     Label3: TLabel;
     Label4: TLabel;
@@ -44,10 +39,19 @@ type
     edCodigo: TEdit;
     edNome: TEdit;
     edRazao: TEdit;
-    BitBtn3: TBitBtn;
-    BitBtn2: TBitBtn;
-    RadioButton5: TRadioButton;
+    GroupBox4: TGroupBox;
+    edCidade: TEdit;
+    BitBtn1: TBitBtn;
+    edtUF: TEdit;
+    Label5: TLabel;
+    edtDDD: TEdit;
+    Label7: TLabel;
+    GroupBox5: TGroupBox;
+    JvDatePickerEdit2: TJvDatePickerEdit;
+    JvDatePickerEdit3: TJvDatePickerEdit;
     procedure BitBtn3Click(Sender: TObject);
+    procedure BitBtn13Click(Sender: TObject);
+    procedure BitBtn2Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -64,17 +68,26 @@ implementation
 procedure TfClienteFiltro.BitBtn3Click(Sender: TObject);
 begin
   //ListaCliente.Close;
-  edCodigo.Text := '';
-  edNome.Text := '';
-  edCidade.Text := '';
-  codVen.Text := '';
-  nomeVen.Text := '';
-  cbtemail.Checked := true;
-  cbttipo.Checked := true;
-  CBox1.Checked := true;
+  edCodigo.Text     := '';
+  edNome.Text       := '';
+  edCidade.Text     := '';
+  codVen.Text       := '';
+  nomeVen.Text      := '';
+  cbtemail.Checked  := true;
+  cbttipo.Checked   := true;
+  CBox1.Checked     := true;
   //RadioButton5.Checked := False;
   //BitBtn2.Click;
+end;
 
+procedure TfClienteFiltro.BitBtn13Click(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TfClienteFiltro.BitBtn2Click(Sender: TObject);
+begin
+  Close;
 end;
 
 end.
