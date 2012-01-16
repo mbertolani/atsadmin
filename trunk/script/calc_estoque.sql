def70fc9067db6922223ced02e1267ef5ae6b9fc
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2012-01-16 15:56:07 +0000

    a Proc. original é a CALCULA_ESTOQUE.

---
+++ trunk/script/calc_estoque.sql
@@ -0,0 +1,41 @@
+SET TERM ^ ;
+CREATE OR ALTER PROCEDURE CALC_ESTOQUE (
+    DATAINI date,
+    DATAFIM date )
+RETURNS (
+    CODPRO integer,
+    DESCPRO varchar(300),
+    QNT double precision,
+    ESTOQUE double precision,
+    SALDO double precision )
+AS
+BEGIN
+    FOR SELECT md.CODPRODUTO, SUM(md.QUANTIDADE), md.DESCPRODUTO
+    FROM MOVIMENTODETALHE md 
+    INNER JOIN MOVIMENTO m on m.CODMOVIMENTO = md.CODMOVIMENTO
+    where md.BAIXA is null and m.CODNATUREZA = 3 and m.DATAMOVIMENTO BETWEEN :DATAINI and :DATAFIM
+    group by md.CODPRODUTO, md.DESCPRODUTO
+    into :CODPRO, :QNT, :DESCPRO
+    do begin    
+    select sum(p.SALDOFIM) from SPESTOQUEPRODUTO(:DATAINI
+                                 , CURRENT_DATE
+                                 , :CODPRO
+                                 , :CODPRO
+                                 , 'TODOS OS GRUPOS CADASTRADOS'
+                                 , 'TODOS SUBGRUPOS DO CADASTRO'
+                                 , 'TODAS AS MARCAS CADASTRADAS'
+                                 , 0) p
+        into :ESTOQUE;
+  --      select p.ESTOQUEATUAL from PRODUTOS p where p.CODPRO = :CODPRO
+    --    INTO :ESTOQUE;
+        SALDO = :ESTOQUE - :QNT;
+        SUSPEND;
+    end
+
+END^
+SET TERM ; ^
+
+
+GRANT EXECUTE
+ ON PROCEDURE CALC_ESTOQUE TO  SYSDBA;
+
