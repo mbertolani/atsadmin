8900da67f6d19e1fea307c3791103b4de8323b57
douglas.darte <douglas.darte@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2012-01-20 18:58:02 +0000

    adição de NF emitidas no periodo por UF

--- trunk/comercial/src/uRelNfe.pas
+++ trunk/comercial/src/uRelNfe.pas
@@ -4,17 +4,20 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, Mask, JvExMask, JvToolEdit, rpcompobase, rpvclreport;
+  Dialogs, StdCtrls, Mask, JvExMask, JvToolEdit, rpcompobase, rpvclreport,
+  Buttons;
 
 type
   TfRelNfe = class(TForm)
     Label5: TLabel;
     Label7: TLabel;
-    Button1: TButton;
     Dta1: TJvDateEdit;
     Dta2: TJvDateEdit;
     VCLReport1: TVCLReport;
-    procedure Button1Click(Sender: TObject);
+    BitBtn2: TBitBtn;
+    BitBtn1: TBitBtn;
+    procedure BitBtn2Click(Sender: TObject);
+    procedure BitBtn1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -30,9 +33,20 @@ uses UDm;
 
 {$R *.dfm}
 
-procedure TfRelNfe.Button1Click(Sender: TObject);
+
+procedure TfRelNfe.BitBtn2Click(Sender: TObject);
 begin
   VCLReport1.Filename := str_relatorio + 'nota_fiscal_rel_emissao.rep';
+  VCLReport1.Title := VCLReport1.Filename;
+  VCLReport1.Report.DatabaseInfo.Items[0].SQLConnection := dm.sqlsisAdimin;
+  VCLReport1.Report.Params.ParamByName('DTAINI').Value := StrToDate(Dta1.Text);
+  VCLReport1.Report.Params.ParamByName('DTAFIM').Value := StrToDate(Dta2.Text);
+  VCLReport1.execute;
+end;
+
+procedure TfRelNfe.BitBtn1Click(Sender: TObject);
+begin
+  VCLReport1.Filename := str_relatorio + 'nota_fiscal_UF.rep';
   VCLReport1.Title := VCLReport1.Filename;
   VCLReport1.Report.DatabaseInfo.Items[0].SQLConnection := dm.sqlsisAdimin;
   VCLReport1.Report.Params.ParamByName('DTAINI').Value := StrToDate(Dta1.Text);
