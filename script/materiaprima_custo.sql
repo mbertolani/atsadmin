f6bbd741361bc22a4a2d2173174f8bd228af6ca9
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2012-01-24 19:20:34 +0000

    Corrigido a falta do suspend.

--- script/materiaprima_custo.sql
+++ script/materiaprima_custo.sql
@@ -1,42 +1,48 @@
 SET TERM ^ ;
-
-CREATE PROCEDURE MATERIAPRIMA_CUSTO
-RETURNS 
- ( codProd integer, 
-    codMat integer, 
-      qtde double precision, 
- custoUnit double precision,
-  custoTot double precision,
-   Produto varchar(300),
-  matPrima varchar(300))
-AS 
+CREATE OR ALTER PROCEDURE MATERIAPRIMA_CUSTO
+RETURNS (
+    CODPROD integer,
+    CODMAT integer,
+    CODPRODPRO varchar(30),
+    CODPRODMAT varchar(30),
+    QTDE double precision,
+    CUSTOUNIT double precision,
+    CUSTOTOT double precision,
+    PRODUTO varchar(300),
+    MATPRIMA varchar(300) )
+AS
 BEGIN
   /* Relatorio com os materiais principais e 
      materias primas com o respectivo preco de custo */
   
   -- Pego todos materiais que tem materia prima cadastrado 
-  for select distinct p.codproduto, p.PRODUTO 
+  for select distinct p.codproduto, p.PRODUTO, p.CODPRO 
         from produtos p, MATERIA_PRIMA m 
        where p.CODPRODUTO = m.CODPRODUTO
          and ((p.USA is null) or (p.USA = 'S')) 
-        into :codProd, :Produto
+        into :codProd, :Produto, :codProdPro
   do begin         
     -- busco a materia prima   
-    for SELECT r.CODPRODMP, r.QTDEUSADA, p.PRODUTO
+    for SELECT r.CODPRODMP, r.QTDEUSADA, p.PRODUTO, p.CODPRO
           FROM MATERIA_PRIMA r, PRODUTOS p
-         WHERE r.CODPRODUTO = p.CODPRODUTO 
+         WHERE r.CODPRODMP  = p.CODPRODUTO 
            AND r.CODPRODUTO = :codProd 
-          INTO :codMat, :qtde, :matPrima
+          INTO :codMat, :qtde, :matPrima, :codProdMat
     do begin
       -- Calcular o Custo de Cada Item 
-      select first 1 m.PRECOCUSTO 
+      select first 1 m.PRECOCOMPRA 
         from ESTOQUEMES m
        where m.CODPRODUTO = :codMat 
        order by m.MESANO DESC  
         into :custoUnit;
-      custoTot = custoUnit * qtde;    
+      custoTot = custoUnit * qtde;  
+      suspend;  
     end 
   end           
 END^
-
 SET TERM ; ^
+
+
+GRANT EXECUTE
+ ON PROCEDURE MATERIAPRIMA_CUSTO TO  SYSDBA;
+
