d1e37333cba22f4c38e13d78dfdff0f3c8c98fca
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2012-02-02 19:45:57 +0000

    Incluido mais uma opção.

--- trunk/comercial/src/uRelNfe.pas
+++ trunk/comercial/src/uRelNfe.pas
@@ -5,19 +5,27 @@ interface
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, StdCtrls, Mask, JvExMask, JvToolEdit, rpcompobase, rpvclreport,
-  Buttons;
+  Buttons, JvMaskEdit, JvCheckedMaskEdit, JvDatePickerEdit;
 
 type
   TfRelNfe = class(TForm)
     Label5: TLabel;
-    Label7: TLabel;
-    Dta1: TJvDateEdit;
-    Dta2: TJvDateEdit;
     VCLReport1: TVCLReport;
     BitBtn2: TBitBtn;
     BitBtn1: TBitBtn;
+    BitBtn3: TBitBtn;
+    GroupBox1: TGroupBox;
+    Label1: TLabel;
+    Label2: TLabel;
+    cbMes: TComboBox;
+    dta1: TJvDatePickerEdit;
+    dta2: TJvDatePickerEdit;
+    btnSair: TBitBtn;
     procedure BitBtn2Click(Sender: TObject);
     procedure BitBtn1Click(Sender: TObject);
+    procedure BitBtn3Click(Sender: TObject);
+    procedure btnSairClick(Sender: TObject);
+    procedure cbMesChange(Sender: TObject);
   private
     { Private declarations }
   public
@@ -29,7 +37,7 @@ var
 
 implementation
 
-uses UDm;
+uses uUtils, UDm;
 
 {$R *.dfm}
 
@@ -52,6 +60,32 @@ begin
   VCLReport1.Report.Params.ParamByName('DTAINI').Value := StrToDate(Dta1.Text);
   VCLReport1.Report.Params.ParamByName('DTAFIM').Value := StrToDate(Dta2.Text);
   VCLReport1.execute;
+end;
+
+procedure TfRelNfe.BitBtn3Click(Sender: TObject);
+begin
+  VCLReport1.Filename := str_relatorio + 'nota_fiscal_rel_emissao_d.rep';
+  VCLReport1.Title := VCLReport1.Filename;
+  VCLReport1.Report.DatabaseInfo.Items[0].SQLConnection := dm.sqlsisAdimin;
+  VCLReport1.Report.Params.ParamByName('DTAINI').Value := StrToDate(Dta1.Text);
+  VCLReport1.Report.Params.ParamByName('DTAFIM').Value := StrToDate(Dta2.Text);
+  VCLReport1.execute;
+end;
+
+procedure TfRelNfe.btnSairClick(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TfRelNfe.cbMesChange(Sender: TObject);
+var  periodo : TUtils;
+begin
+  periodo := TUtils.Create;
+  periodo.criaIni(cbMes.text);
+  periodo.criaFim(cbMes.text);
+  dta1.Text := periodo.PeriodoIni;
+  dta2.Text := periodo.PeriodoFim;
+  periodo.Destroy;
 end;
 
 end.
