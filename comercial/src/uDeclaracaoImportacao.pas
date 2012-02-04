536b32263d2c4821ffdcbdae914268f15fc3f8b7
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2012-02-04 14:33:44 +0000

    Bugs.

--- comercial/src/uDeclaracaoImportacao.pas
+++ comercial/src/uDeclaracaoImportacao.pas
@@ -45,14 +45,23 @@ type
     Button1: TButton;
     DBEdit7: TDBEdit;
     JvDBUltimGrid1: TJvDBUltimGrid;
+    sdsDINOTASERIE: TStringField;
+    sdsDICODMOVIMENTO: TIntegerField;
+    cdsDINOTASERIE: TStringField;
+    cdsDICODMOVIMENTO: TIntegerField;
     procedure btnGravarClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure Button1Click(Sender: TObject);
     procedure FormShow(Sender: TObject);
     procedure btnExcluirClick(Sender: TObject);
+    procedure btnIncluirClick(Sender: TObject);
+    procedure DBEdit6Exit(Sender: TObject);
   private
     { Private declarations }
   public
+    notaSerieDec    : String;
+    codMovimentoDec : Integer;
+    codExportadorDec: Integer;
     { Public declarations }
   end;
 
@@ -73,28 +82,43 @@ begin
   TD.IsolationLevel := xilREADCOMMITTED;
   dm.sqlsisAdimin.StartTransaction(TD);
   if cdsDI.State in [dsInsert] then
-   begin
+  begin
     if dm.c_6_genid.Active then
       dm.c_6_genid.Close;
     dm.c_6_genid.CommandText := 'SELECT CAST(GEN_ID(GEN_DI, 1) AS INTEGER) AS CODIGO FROM RDB$DATABASE';
     dm.c_6_genid.Open;
     cdsDIDI_CODDI.AsInteger := dm.c_6_genid.Fields[0].AsInteger;
     dm.c_6_genid.Close;
-    cm :=  'INSERT INTO DECLARACAOIMPORTACAO (DI_CODDI, DI_NUMDI, DI_DATA, DI_LOCALDESEMB, DI_UFDESEMB, DI_DATADESEMB, DI_CODEXPORTADOR) ' +
-    'VALUES (' + IntToStr(cdsDIDI_CODDI.AsInteger) + ', ' + QuotedStr(cdsDIDI_NUMDI.AsString) + ', ' + QuotedStr(FormatDateTime('mm/dd/yyyy', cdsDIDI_DATA.AsDateTime)) + ', ' +
-    QuotedStr(cdsDIDI_LOCALDESEMB.AsString) + ', ' + QuotedStr(cdsDIDI_UFDESEMB.AsString) + ', ' + QuotedStr(FormatDateTime('mm/dd/yyyy', cdsDIDI_DATADESEMB.AsDateTime)) + ', ' + IntToStr(cdsDIDI_CODEXPORTADOR.AsInteger) + ')';
-   end
-   else
-   begin
-     cm := 'UPDATE DECLARACAOIMPORTACAO SET DI_NUMDI = ' + QuotedStr(cdsDIDI_NUMDI.AsString) + ', DI_DATA = ' + QuotedStr(FormatDateTime('mm/dd/yyyy', cdsDIDI_DATA.AsDateTime)) +
-     ', DI_LOCALDESEMB = ' + QuotedStr(cdsDIDI_LOCALDESEMB.AsString) + ', DI_UFDESEMB = ' + QuotedStr(cdsDIDI_UFDESEMB.AsString) +
-     ', DI_DATADESEMB = ' + QuotedStr(FormatDateTime('mm/dd/yyyy', cdsDIDI_DATADESEMB.AsDateTime)) + ', DI_CODEXPORTADOR = ' + IntToStr(cdsDIDI_CODEXPORTADOR.AsInteger) +
-     ' WHERE DI_CODDI =  ' + IntToStr(cdsDIDI_CODDI.AsInteger);
-   end;
+    cm :=  'INSERT INTO DECLARACAOIMPORTACAO (' +
+    ' DI_CODDI, DI_NUMDI, DI_DATA, DI_LOCALDESEMB, DI_UFDESEMB, DI_DATADESEMB, ' +
+    ' DI_CODEXPORTADOR, NOTASERIE, CODMOVIMENTO) ' +
+    'VALUES (' +
+    IntToStr(cdsDIDI_CODDI.AsInteger) + ', ' +
+    QuotedStr(cdsDIDI_NUMDI.AsString) + ', ' +
+    QuotedStr(FormatDateTime('mm/dd/yyyy', cdsDIDI_DATA.AsDateTime)) + ', ' +
+    QuotedStr(cdsDIDI_LOCALDESEMB.AsString) + ', ' +
+    QuotedStr(cdsDIDI_UFDESEMB.AsString) + ', ' +
+    QuotedStr(FormatDateTime('mm/dd/yyyy', cdsDIDI_DATADESEMB.AsDateTime)) + ', ' +
+    IntToStr(cdsDIDI_CODEXPORTADOR.AsInteger) + ', ' +
+    QuotedStr(notaSerieDec) + ', ' +
+    IntToStr(codMovimentoDec) + ')';
+  end
+  else
+  begin
+    cm := 'UPDATE DECLARACAOIMPORTACAO SET ' +
+    '          DI_NUMDI = ' + QuotedStr(cdsDIDI_NUMDI.AsString) +
+    ',          DI_DATA = ' + QuotedStr(FormatDateTime('mm/dd/yyyy', cdsDIDI_DATA.AsDateTime)) +
+    ',   DI_LOCALDESEMB = ' + QuotedStr(cdsDIDI_LOCALDESEMB.AsString) +
+    ',      DI_UFDESEMB = ' + QuotedStr(cdsDIDI_UFDESEMB.AsString) +
+    ',    DI_DATADESEMB = ' + QuotedStr(FormatDateTime('mm/dd/yyyy', cdsDIDI_DATADESEMB.AsDateTime)) +
+    ', DI_CODEXPORTADOR = ' + IntToStr(cdsDIDI_CODEXPORTADOR.AsInteger) +
+    ',        NOTASERIE = ' + QuotedStr(notaSerieDec) +
+    ',     CODMOVIMENTO = ' + IntToStr(codMovimentoDec) +
+    ' WHERE DI_CODDI =  ' + IntToStr(cdsDIDI_CODDI.AsInteger);
+  end;
   dm.sqlsisAdimin.ExecuteDirect(cm);
   dm.sqlsisAdimin.Commit(TD);
   inherited;
-
 end;
 
 procedure TfDeclaracaoImportacao.FormCreate(Sender: TObject);
@@ -143,6 +167,40 @@ begin
   cm := 'delete from DECLARACAOIMPORTACAO where DI_CODDI = ' + IntToStr(cdsDIDI_CODDI.AsInteger);
   dm.sqlsisAdimin.ExecuteDirect(cm);
   dm.sqlsisAdimin.Commit(TD);
+end;
+
+procedure TfDeclaracaoImportacao.btnIncluirClick(Sender: TObject);
+begin
+  inherited;
+  cdsDIDI_CODEXPORTADOR.AsInteger := codExportadorDec;
+  if dm.scds_forn_proc.Active then
+  dm.scds_forn_proc.Close;
+  dm.scds_forn_proc.Params[0].Clear;
+  dm.scds_forn_proc.Params[1].Clear;
+  dm.scds_forn_proc.Params[2].AsInteger := codExportadorDec;
+  dm.scds_forn_proc.Params.ParamByName('pStatus').AsInteger := 1;
+  dm.scds_forn_proc.Params.ParamByName('pSegmento').AsInteger := 1;
+  dm.scds_forn_proc.Open;
+  cdsDINOMEFORNECEDOR.AsString := dm.scds_forn_procNOMEFORNECEDOR.AsString;
+end;
+
+procedure TfDeclaracaoImportacao.DBEdit6Exit(Sender: TObject);
+begin
+  inherited;
+  if dm.scds_forn_proc.Active then
+  dm.scds_forn_proc.Close;
+  dm.scds_forn_proc.Params[0].Clear;
+  dm.scds_forn_proc.Params[1].Clear;
+  dm.scds_forn_proc.Params[2].AsInteger:=StrToInt(DBEdit6.Text);
+  dm.scds_forn_proc.Params.ParamByName('pStatus').AsInteger := 1;
+  dm.scds_forn_proc.Params.ParamByName('pSegmento').AsInteger := 1;
+  dm.scds_forn_proc.Open;
+  if dm.scds_forn_proc.IsEmpty then begin
+    MessageDlg('Código não cadastrado.', mtWarning,
+    [mbOk], 0);
+    exit;
+  end;
+  cdsDINOMEFORNECEDOR.AsString := dm.scds_forn_procNOMEFORNECEDOR.AsString;
 end;
 
 end.
