f4ea4fa30def5086d0a1806b85c9c06c7657c020
douglas.darte <douglas.darte@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2012-03-06 18:45:49 +0000

    correção para bloquear alteração ou exclusão da NF caso tenha sido enviada para a receita

--- trunk/script/proibe_alt_del_nf.sql
+++ trunk/script/proibe_alt_del_nf.sql
@@ -2,6 +2,7 @@ CREATE OR ALTER TRIGGER PROIBE_ALT_DEL_NF FOR NOTAFISCAL ACTIVE
 BEFORE UPDATE OR DELETE POSITION 0
 as 
 BEGIN 
-  if ((old.PROTOCOLOENV is not null) or (old.PROTOCOLOCANC is not null)) then
-    exception ALTERA_NFE;	
+	if ( (old.PROTOCOLOENV is not null) and (new.PROTOCOLOENV is not null) ) then
+		if (old.PROTOCOLOCANC = new.PROTOCOLOCANC) then
+			exception ALTERA_NFE;
 END
