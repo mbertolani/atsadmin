9f1db50013717f3d0041b829cb0972a4ec35849b
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2012-03-07 20:17:12 +0000

    Desenvolvimento;

--- teste/uPagarTeste.pas
+++ teste/uPagarTeste.pas
@@ -13,13 +13,13 @@ type
 
   published
     procedure TestPagInclusao;
-    procedure TestPagAlteracao;
+    procedure TestPagBaixarTitulo;
     procedure TestPagExclusao;
 end;
 
-
-
 implementation
+
+uses SysUtils, UDm, DateUtils;
 
 { TPagTeste }
 
@@ -36,18 +36,43 @@ begin
 
 end;
 
-procedure TPagTeste.TestPagAlteracao;
+procedure TPagTeste.TestPagBaixarTitulo;
 begin
 
 end;
 
 procedure TPagTeste.TestPagExclusao;
 begin
+ // Testa exclusão
+  FPag.excluiTitulo(68);
 
+  dm.sqlBusca.Close;
+  dm.sqlBusca.SQL.Clear;
+  dm.sqlBusca.SQL.Add('SELECT CODPAGAMENTO ' +
+    ' FROM PAGAMENTO ' +
+    'WHERE CODPAGAMENTO  = ' + IntToStr(68));
+
+  dm.sqlBusca.Open;
+  if (dm.sqlBusca.IsEmpty) then
+    check(1 = 1 , 'TITULO Excluído.')
+  else
+    check(1 = 2 , 'Titulo não Excluído.')
 end;
 
 procedure TPagTeste.TestPagInclusao;
+var codPag, codCompra: Integer;
 begin
+ CodCompra       := 68;  // ESTE Códito é necessário para Gerar um título para a Compra
+
+ codPag := FPag.geraTitulo(0, codCompra);
+
+  dm.sqlBusca.Close;
+  dm.sqlBusca.SQL.Clear;
+  dm.sqlBusca.SQL.Add('SELECT CODCompra' +
+        '  FROM PAGAMENTO R ' +
+        ' WHERE R.CODPAGAMENTO = ' + IntToStr(codPag));
+  dm.sqlBusca.Open;
+  check(dm.sqlBusca.FieldByName('CodCompra').AsInteger = codCompra , 'Título Não Gravado.');
 
 end;
 
