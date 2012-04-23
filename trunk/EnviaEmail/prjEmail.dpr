3017be7ddfc1983f4d7613b96d0b7b6f2ad02966
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2012-04-23 13:57:01 +0000

    Finalizado o envio de emaail pelo ats admin.

--- trunk/EnviaEmail/prjEmail.dpr
+++ trunk/EnviaEmail/prjEmail.dpr
@@ -3,7 +3,10 @@ program prjEmail;
 uses
   Forms,
   uEmail in 'uEmail.pas' {Form1},
-  UDm in '..\comercial\src\UDm.pas' {DM: TDataModule};
+  UDm in '..\comercial\src\UDm.pas' {DM: TDataModule},
+  uUtils in '..\comercial\src\uUtils.pas',
+  uDmApp in '..\comercial\src\udmapp.pas' {dmApp: TDataModule},
+  md5 in '..\comercial\src\md5.pas';
 
 {$R *.res}
 
@@ -11,5 +14,6 @@ begin
   Application.Initialize;
   Application.CreateForm(TDM, DM);
   Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TdmApp, dmApp);
   Application.Run;
 end.
