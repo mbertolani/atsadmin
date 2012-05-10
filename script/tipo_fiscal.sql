7cdc821bf197ab9fb177224bc8534c6804488d0d
douglas.darte <douglas.darte@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2012-05-10 13:59:41 +0000

    Classificação Fiscal

---
+++ script/tipo_fiscal.sql
@@ -0,0 +1,32 @@
+CREATE DOMAIN T_FISCAL AS char(1) COLLATE WIN1252;
+
+commit;
+
+create table TIPO_FISCAL(
+    CODFISCAL T_FISCAL not null primary key,
+    DESCRICAO varchar(60) not null);
+
+commit;
+
+INSERT INTO TIPO_FISCAL (CODFISCAL, DESCRICAO) VALUES ('F', 'Físico Contribuinte');
+INSERT INTO TIPO_FISCAL (CODFISCAL, DESCRICAO) VALUES ('J', 'Jurídico Contribuinte');
+INSERT INTO TIPO_FISCAL (CODFISCAL, DESCRICAO) VALUES ('G', 'Físico Não Contribuinte');
+INSERT INTO TIPO_FISCAL (CODFISCAL, DESCRICAO) VALUES ('K', 'Jurídico Não Contribuinte');
+INSERT INTO TIPO_FISCAL (CODFISCAL, DESCRICAO) VALUES ('H', 'Físico Simples');
+INSERT INTO TIPO_FISCAL (CODFISCAL, DESCRICAO) VALUES ('L', 'Jurídico Simples');
+
+commit;
+
+ALTER TABLE CLIENTES add CODFISCAL T_FISCAL;
+alter table CLIENTES add constraint FK_TFISCAL2 foreign key (CODFISCAL)  references TIPO_FISCAL (CODFISCAL) on update NO ACTION on delete NO ACTION;
+ALTER TABLE FORNECEDOR add CODFISCAL T_FISCAL;
+alter table FORNECEDOR add constraint FK_TFISCAL3 foreign key (CODFISCAL)  references TIPO_FISCAL (CODFISCAL) on update NO ACTION on delete NO ACTION;
+ALTER TABLE ESTADO_ICMS add CODFISCAL T_FISCAL;
+alter table ESTADO_ICMS add constraint FK_TFISCAL foreign key (CODFISCAL) references TIPO_FISCAL (CODFISCAL) on update NO ACTION on delete NO ACTION;
+
+commit;
+
+update ESTADO_ICMS set CODFISCAL = UDF_LEFT(PESSOA, 1);
+
+
+commit;
