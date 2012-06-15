59cc6aae6ba653611982bbd82e7f35c3886a05bd
douglas.darte <douglas.darte@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2012-06-15 16:46:35 +0000

    carregar indice de pagamento e nitemped e pedido no detalhe para NFe

--- script/gera_nf_venda.sql
+++ script/gera_nf_venda.sql
@@ -69,6 +69,9 @@ as
   declare variable tFRETE integer;
   declare variable CODTRANSPORTADORA INTEGER;
   declare variable Entrada double precision;
+  declare variable xped varchar(20);
+  declare variable nitemped integer;
+  declare variable indpag integer;
     
 begin 
 
@@ -117,11 +120,11 @@ begin
   into :codMovNovo;
 
   -- insiro o Movimento   
-  for Select mov.CODALMOXARIFADO, mov.CODUSUARIO, mov.CODVENDEDOR, ven.N_PARCELA, ven.PRAZO, ven.VALOR_FRETE, mov.CODTRANSP, mov.TPFRETE, ven.ENTRADA
+  for Select mov.CODALMOXARIFADO, mov.CODUSUARIO, mov.CODVENDEDOR, ven.N_PARCELA, ven.PRAZO, ven.VALOR_FRETE, mov.CODTRANSP, mov.TPFRETE, ven.ENTRADA, mov.CODPEDIDO
     from movimento mov 
     inner join venda ven on ven.CODMOVIMENTO = mov.CODMOVIMENTO 
     where mov.CODMOVIMENTO = :codMov
-  into :codCCusto, :codUser, :codVendedor, :np, :PRAZO, :vFreteT, :CODTRANSPORTADORA, :tpfrete, :entrada
+  into :codCCusto, :codUser, :codVendedor, :np, :PRAZO, :vFreteT, :CODTRANSPORTADORA, :tpfrete, :entrada, :xped
   do begin 
     insert into movimento (codmovimento, codcliente, codAlmoxarifado, codUsuario
       , codVendedor, dataMovimento, status, codNatureza, controle, codtransp) 
@@ -129,6 +132,7 @@ begin
         , :codVendedor, :dtEmissao, 0, 15, :codMov, :CODTRANSPORTADORA);  
   end 
     pesoTotal = 0;
+    nitemped = 0;
     -- localiza o mov. detalhe
     for select  md.QTDE_ALT, md.CODPRODUTO, md.QUANTIDADE, md.UN, md.PRECO, md.DESCPRODUTO
       , md.ICMS, prod.BASE_ICMS, prod.PESO_QTDE , prod.CST, md.OBS
@@ -137,6 +141,7 @@ begin
       where md.CODMOVIMENTO = :codMov
     into :desconto, :codProduto, :qtde, :un, :preco, :descP, :icms, :baseIcms, :pesoUn, :cstProd, :obsp
     do begin 
+     nitemped = nitemped + 1;
 
      if (pesoUn is null) then 
         pesoUn = 0;
@@ -178,28 +183,20 @@ begin
       if (uf = 'SP') then 
       begin 
         valoricms = (preco * qtde) * (baseIcms / 100) * (icms / 100); 
-        --tBaseIcms = tBaseIcms + (icms/100);
       end  
-      -- Calculo ICMS outros ESTADOS 
       if (uf <> 'SP') then 
       begin 
-        /*select first 1 icms, reducao from ESTADO_ICMS where uf = :uf and cfop = :cfop
-        into :icms, :baseIcms;
-        if (icms is null) then 
-          icms = 0;*/
     
         if (baseIcms is null) then 
           baseIcms = 0;
-
         valoricms = (preco * qtde) * (baseIcms / 100) * (icms / 100); 
-        --tBaseIcms = tBaseIcms + (icms/100);
       end  
           
       insert into MOVIMENTODETALHE (codDetalhe, codMovimento, codProduto, quantidade
-       , preco, un, descProduto, icms, valor_icms, cst, qtde_alt, VALOR_DESCONTO, vlr_base, II, BCII, OBS) 
+       , preco, un, descProduto, icms, valor_icms, cst, qtde_alt, VALOR_DESCONTO, vlr_base, II, BCII, OBS, NITEMPED, PEDIDO) 
       values(gen_id(GENMOVDET, 1), :codMovNovo, :codProduto, :qtde
-       , :preco, :un, :descP, :icms, :valoricms, :cst,  :desconto, ((:qtde * :preco)*(:desconto/100)),(:preco-((:preco)*(:desconto/100))), 0, 0, :obsp);  
-      total = total + (qtde * (:preco*(1-(:desconto/100))));--((:PRECO/:np) * :desconto)); --((:PRECO/:np) * :desconto)
+       , :preco, :un, :descP, :icms, :valoricms, :cst,  :desconto, ((:qtde * :preco)*(:desconto/100)),(:preco-((:preco)*(:desconto/100))), 0, 0, :obsp, :nitemped, :xped);  
+      total = total + (qtde * (:preco*(1-(:desconto/100))));
       totalIcms = totalIcms + :valoricms;
     end 
     vIcmsT = 0; 
@@ -224,6 +221,11 @@ begin
   begin
     tBaseIcms = 0; 
   end
+  
+  if (:total = :entrada) then
+    indpag = 0;
+  else
+    indpag = 1;
   
   SELECT FIRST 1 UDF_LEFT(ei.DADOSADC1,200), UDF_LEFT(ei.DADOSADC2,200), UDF_LEFT(ei.DADOSADC3,200), 
      UDF_LEFT(ei.DADOSADC4,200), UDF_LEFT(ei.DADOSADC5,75), UDF_LEFT(ei.DADOSADC6,75) 
@@ -254,24 +256,28 @@ begin
   select GEN_ID(GEN_NF, 1) from RDB$DATABASE
   into :codNF;
   
-  if( TPFRETE = 'E') then
+  if( TPFRETE = '0') then
+   TFRETE = 0;
+  if( TPFRETE = '1') then
    TFRETE = 1;
-  if( TPFRETE = 'D') then
+  if( TPFRETE = '2') then
    TFRETE = 2;
+  if( TPFRETE = '3') then
+   TFRETE = 3;   
 
   INSERT INTO NOTAFISCAL (NOTASERIE, NUMNF, NATUREZA, codVenda, codCliente, cfop
     , valor_total_nota, dtaEmissao, VALOR_ICMS, BASE_ICMS_SUBST, VALOR_ICMS_SUBST
     , VALOR_FRETE, VALOR_PRODUTO, VALOR_SEGURO, OUTRAS_DESP, VALOR_IPI, BASE_ICMS, NOTAFISCAL
     , NOMETRANSP, PLACATRANSP, CNPJ_CPF, END_TRANSP , CIDADE_TRANSP, UF_VEICULO_TRANSP
     , UF_TRANSP, FRETE, INSCRICAOESTADUAL, CORPONF1, CORPONF2, CORPONF3, CORPONF4, CORPONF5
-    , CORPONF6, PESOBRUTO, PESOLIQUIDO, SERIE, UF, VALOR_DESCONTO, II, BCII)
+    , CORPONF6, PESOBRUTO, PESOLIQUIDO, SERIE, UF, VALOR_DESCONTO, II, BCII, INDPAG)
     VALUES (:numero, :codNF, 15, :codVen, :Cliente, :cfop
     , :total, :dtEmissao, :totalIcms, 0 , 0
     , :vFreteT, :preco, :vSeguroT, :vOutrosT, :vIpiT, :tBaseIcms ,:numero
     , :NOMETRANSP, :PLACATRANSP, :CNPJ_CPF, :END_TRANSP
     , :CIDADE_TRANSP, :UF_VEICULO_TRANSP, :UF_TRANSP, :TFRETE, :INSCRICAOESTADUAL
     , :CORPONF1, :CORPONF2, :CORPONF3, :CORPONF4, :CORPONF5, :CORPONF6, :pesoTotal, :pesoTotal
-    , :serie, :UF, 0, 0, 0);
+    , :serie, :UF, 0, 0, 0, :indpag);
  
    -- Faço um select para saber o valor gerado da nf, pois, existe uma trigger q muda o vlr
    -- da nf qdo esta e parcelada (dnz)
