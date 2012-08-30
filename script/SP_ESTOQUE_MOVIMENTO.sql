1e2a892fc6cd1b5872f88365e592db45dc217052
crsilveira <crsilveira@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2012-08-30 11:41:24 +0000

    Atualizado para imprimir somente itens com movimento.

--- script/SP_ESTOQUE_MOVIMENTO.sql
+++ script/SP_ESTOQUE_MOVIMENTO.sql
@@ -1,44 +1,37 @@
-SET TERM ^;
+set term ^;
 CREATE OR ALTER PROCEDURE SP_ESTOQUE_MOVIMENTO (
-    DTA1 Date,
-    DTA2 Date,
-    MARC Varchar(50) )
+    DTA1 date,
+    DTA2 date,
+    MARC integer )
 RETURNS (
-    CODPRO Varchar(20),
-    CODPRODUTO INTEGER,
-    PRODUTO Varchar(200),
-    EST_ATUAL Double precision,
-    ENTRADA Double precision,
-    SAIDA Double precision,
-    PRECOMEDIO Double precision,
-    UN CHAR(2),
-    MARCA Varchar(50))
+    CODPRO varchar(20),
+    CODPRODUTO integer,
+    PRODUTO varchar(300),
+    EST_ATUAL double precision,
+    ENTRADA double precision,
+    SAIDA double precision,
+    PRECOMEDIO double precision,
+    UN char(2),
+    MARCA varchar(50),
+    CENTRORESULTADO VARCHAR(100) )
 AS
-
 BEGIN
-
-        FOR SELECT prod.CODPRODUTO, prod.PRODUTO, prod.ESTOQUEATUAL, prod.PRECOMEDIO, 
-        prod.CODPRO, prod.UNIDADEMEDIDA, prod.MARCA
-        FROM PRODUTOS prod 
-        WHERE ((prod.MARCA = :MARC) OR (:MARC = 'TODAS AS MARCAS CADASTRADAS'))
-        INTO :CODPRODUTO, :PRODUTO, :EST_ATUAL , :PRECOMEDIO, :CODPRO, :UN, :MARCA
-
-        DO BEGIN
-            --COMPRA / ENTRADA
-            SELECT SUM(md.QUANTIDADE) as QUANTIDADE FROM MOVIMENTODETALHE md
-            INNER JOIN MOVIMENTO m on m.CODMOVIMENTO = md.CODMOVIMENTO 
-            where md.CODPRODUTO = :CODPRODUTO and md.BAIXA = 0 and (m.DATAMOVIMENTO BETWEEN :DTA1 AND :DTA2)
-            into :ENTRADA;
-            
-            --VENDA / SAIDA
-            SELECT SUM(md.QUANTIDADE) as QUANTIDADE FROM MOVIMENTODETALHE md
-            INNER JOIN MOVIMENTO m on m.CODMOVIMENTO = md.CODMOVIMENTO 
-            where md.CODPRODUTO = :CODPRODUTO and md.BAIXA = 1 and (m.DATAMOVIMENTO BETWEEN :DTA1 AND :DTA2)
-            into :SAIDA;
-            
-            SUSPEND;
- 
+  select nome from plano  where codigo = :marc
+  into :CENTRORESULTADO;
+    FOR SELECT prod.CODPRODUTO, prod.PRODUTO, prod.ESTOQUEATUAL, prod.PRECOMEDIO, 
+    prod.CODPRO, prod.UNIDADEMEDIDA, prod.MARCA
+    FROM PRODUTOS prod 
+    INTO :CODPRODUTO, :PRODUTO, :EST_ATUAL , :PRECOMEDIO, :CODPRO, :UN, :MARCA
+    DO BEGIN
+        ENTRADA = 0;
+        SAIDA = 0;
+        EST_ATUAL = 0;
+        select first 1 (coalesce(em.QTDECOMPRA,0) + coalesce(em.QTDEENTRADA,0)), (coalesce(em.QTDEVENDA,0) + coalesce(em.QTDESAIDA,0)), em.SALDOESTOQUE from ESTOQUEMES em 
+        where em.CODPRODUTO = :CODPRODUTO and em.CENTROCUSTO = :MARC
+        order by em.MESANO desc
+        into :ENTRADA, :SAIDA, :EST_ATUAL;
+        
+        SUSPEND;
     END
 
 END
-
