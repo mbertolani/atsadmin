fa8b2a7fa2b9111d1cad14fc41900e94354f2330
ats.manoel <ats.manoel@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2012-11-27 16:22:20 +0000

    Cliente Cia da Memoria

---
+++ trunk/comercial/src/uFiltroCadDoc.pas
@@ -0,0 +1,109 @@
+unit uFiltroCadDoc;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, FMTBcd, DB, Grids, DBGrids, SqlExpr, Provider, DBClient,
+  StdCtrls, Buttons, EDBFind, Mask, JvExMask, JvToolEdit, JvMaskEdit,
+  JvCheckedMaskEdit, JvDatePickerEdit;
+
+type
+  TfFiltroCadDoc = class(TForm)
+    btnvoltar: TBitBtn;
+    cdsCad: TClientDataSet;
+    dspCa: TDataSetProvider;
+    dsCad: TDataSource;
+    sqlCad: TSQLDataSet;
+    DBGrid1: TDBGrid;
+    sqlCadID: TIntegerField;
+    sqlCadCODDOC: TStringField;
+    sqlCadDESCR: TStringField;
+    sqlCadANODESC: TFloatField;
+    sqlCadMETODO: TStringField;
+    sqlCadLEI1: TStringField;
+    sqlCadLEI2: TStringField;
+    sqlCadOBS: TStringField;
+    sqlCadSEQUENCIA: TStringField;
+    sqlCadMICROF: TStringField;
+    sqlCadDIGITA: TStringField;
+    sqlCadPRAZO: TStringField;
+    sqlCadFORDESTR: TStringField;
+    sqlCadFISCAL: TStringField;
+    sqlCadANEXO: TMemoField;
+    sqlCadCODSIS: TStringField;
+    cdsCadID: TIntegerField;
+    cdsCadCODDOC: TStringField;
+    cdsCadDESCR: TStringField;
+    cdsCadANODESC: TFloatField;
+    cdsCadMETODO: TStringField;
+    cdsCadLEI1: TStringField;
+    cdsCadLEI2: TStringField;
+    cdsCadOBS: TStringField;
+    cdsCadSEQUENCIA: TStringField;
+    cdsCadMICROF: TStringField;
+    cdsCadDIGITA: TStringField;
+    cdsCadPRAZO: TStringField;
+    cdsCadFORDESTR: TStringField;
+    cdsCadFISCAL: TStringField;
+    cdsCadANEXO: TMemoField;
+    cdsCadCODSIS: TStringField;
+    EvDBFind1: TEvDBFind;
+    EvDBFind2: TEvDBFind;
+    BitBtn1: TBitBtn;
+    hoje: TJvDatePickerEdit;
+    procedure btnvoltarClick(Sender: TObject);
+    procedure BitBtn1Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  fFiltroCadDoc: TfFiltroCadDoc;
+
+implementation
+
+uses UDm, uCadDoc, uCorreio;
+
+{$R *.dfm}
+
+procedure TfFiltroCadDoc.btnvoltarClick(Sender: TObject);
+begin
+  if(FCadDoc.cdsC.Active) then
+    FCadDoc.cdsC.Close;
+    FCadDoc.cdsC.Params[0].AsInteger := cdsCadID.AsInteger;
+  FCadDoc.cdsC.Open;
+  fFiltroCadDoc.Close;
+end;
+
+procedure TfFiltroCadDoc.BitBtn1Click(Sender: TObject);
+var data ,soma : String;
+ dia, mes, ano : word;
+
+begin
+  data := hoje.Text;
+  DecodeDate(StrToDate(data),ano,mes,dia);
+  ano := (ano + StrToInt(cdsCadANODESC.Text));
+
+  if(fCorreio.DtSrc.DataSet.State in [dsInsert]) then
+  begin
+    fCorreio.DBEdit9.Text := cdsCadCODDOC.Text;
+    fCorreio.DBEdit16.Text := (IntToStr(mes) + IntToStr(ano));
+    fCorreio.Edit1.Text := cdsCadDESCR.Text;
+
+  end;
+  fFiltroCadDoc.Close;
+end;
+
+procedure TfFiltroCadDoc.FormShow(Sender: TObject);
+begin
+  if(cdsCad.Active) then
+  cdsCad.Close;
+  cdsCad.Open;
+
+end;
+
+end.
