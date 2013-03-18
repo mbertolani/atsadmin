18a9f23049cb1a63f185f55d51c807f2c3233f26
crsilveira@gmail.com <crsilveira@gmail.com@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2013-03-18 12:01:12 +0000

    Organizado o SQL

--- trunk/script/listaSpEstoqueFiltro118.sql
+++ trunk/script/listaSpEstoqueFiltro118.sql
@@ -1,3 +1,4 @@
+set term ^;
 CREATE OR ALTER PROCEDURE  LISTASPESTOQUEFILTRO( DTA1                             DATE
                                 , DTA2                             DATE
                                 , PROD1                            INTEGER
@@ -49,7 +50,7 @@ BEGIN
     saida = 0;
     acumula = 0;
     /* SAIDA */
-    FOR SELECT l.codlote, mov.datamovimento, mov.CODMOVIMENTO, natu.BAIXAMOVIMENTO, natu.DESCNATUREZA, movdet.CODPRODUTO, 
+    FOR SELECT distinct l.codlote, mov.datamovimento, mov.CODMOVIMENTO, natu.BAIXAMOVIMENTO, natu.DESCNATUREZA, movdet.CODPRODUTO, 
         movdet.LOTE, movdet.DTAFAB, movdet.DTAVCTO, mov.CODALMOXARIFADO, prod.CODPRO, prod.PRODUTO, prod.FAMILIA, prod.CATEGORIA, mov.OBS, prod.VALORUNITARIOATUAL, 
         prod.VALOR_PRAZO
         FROM MOVIMENTO mov
@@ -59,13 +60,18 @@ BEGIN
         inner join PRODUTOS prod on movdet.codproduto = prod.codproduto 
         left outer join lotes l on movdet.lote = l.lote and l.codproduto = movdet.codproduto
 
-        WHERE (v.DATAVENDA BETWEEN :DTA1 AND :DTA2) AND (movdet.CODPRODUTO BETWEEN :PROD1 AND :PROD2)  and ((prod.TIPO <> 'SERV') or (prod.tipo is null)) AND ((mov.CODNATUREZA = :NATUREZA) OR (:NATUREZA = 100)) AND
-            ((mov.CODALMOXARIFADO = :CCUSTO) OR (:CCUSTO = 1))  and ((movdet.LOTE = :LOTE) or (:LOTE = 'TODOS OS LOTES CADASTRADOS NO SISTEMA'))
-            AND ((prod.CATEGORIA = :SUBGRUPO) OR (:SUBGRUPO = 'TODOS SUBGRUPOS DO CADASTRO CATEGORIA'))  
-            AND ((prod.MARCA = :MARCA) OR (:MARCA = 'TODAS AS MARCAS CADASTRADAS NO SISTEMA')) and movdet.BAIXA is not null 
-            AND ((prod.FAMILIA = :GRUPOPROC) OR (:GRUPOPROC = 'TODOS OS GRUPOS CADASTRADOS NO SISTEMA'))
-
-            group by  prod.FAMILIA, prod.CATEGORIA, prod.CODPRO, prod.PRODUTO, movdet.CODPRODUTO, l.codlote, movdet.LOTE
+        WHERE (v.DATAVENDA BETWEEN :DTA1 AND :DTA2) 
+          AND (movdet.CODPRODUTO BETWEEN :PROD1 AND :PROD2)  
+          AND ((prod.TIPO <> 'SERV') or (prod.tipo is null)) 
+          AND ((mov.CODNATUREZA = :NATUREZA) OR (:NATUREZA = 100)) 
+          AND ((mov.CODALMOXARIFADO = :CCUSTO) OR (:CCUSTO = 1))  
+          AND ((movdet.LOTE = :LOTE) or (:LOTE = 'TODOS OS LOTES CADASTRADOS NO SISTEMA'))
+          AND ((prod.CATEGORIA = :SUBGRUPO) OR (:SUBGRUPO = 'TODOS SUBGRUPOS DO CADASTRO CATEGORIA'))  
+          AND ((prod.MARCA = :MARCA) OR (:MARCA = 'TODAS AS MARCAS CADASTRADAS NO SISTEMA')) 
+          AND movdet.BAIXA is not null
+          AND natu.BAIXAMOVIMENTO = 1
+          AND ((prod.FAMILIA = :GRUPOPROC) OR (:GRUPOPROC = 'TODOS OS GRUPOS CADASTRADOS NO SISTEMA'))
+          group by  prod.FAMILIA, prod.CATEGORIA, prod.CODPRO, prod.PRODUTO, movdet.CODPRODUTO, l.codlote, movdet.LOTE
                 , mov.datamovimento, mov.codNatureza
                 , natu.BAIXAMOVIMENTO, natu.DESCNATUREZA
                 , mov.CODMOVIMENTO, movdet.DTAFAB, 
@@ -81,7 +87,7 @@ BEGIN
         SUSPEND;
     end    
     /* ENTRADA */
-    FOR SELECT l.codlote, mov.datamovimento, mov.CODMOVIMENTO, natu.BAIXAMOVIMENTO, natu.DESCNATUREZA, movdet.CODPRODUTO, 
+    FOR SELECT distinct l.codlote, mov.datamovimento, mov.CODMOVIMENTO, natu.BAIXAMOVIMENTO, natu.DESCNATUREZA, movdet.CODPRODUTO, 
         movdet.LOTE, movdet.DTAFAB, movdet.DTAVCTO, mov.CODALMOXARIFADO, prod.CODPRO, prod.PRODUTO, prod.FAMILIA, prod.CATEGORIA, mov.OBS
         , prod.VALORUNITARIOATUAL, prod.VALOR_PRAZO
         FROM MOVIMENTO mov
@@ -90,14 +96,18 @@ BEGIN
         inner join MOVIMENTODETALHE  movdet on  movdet.CODMOVIMENTO = mov.CODMOVIMENTO
         inner join PRODUTOS prod on movdet.codproduto = prod.codproduto 
         left outer join lotes l on movdet.lote = l.lote and l.codproduto = movdet.codproduto
-
-        WHERE (v.DATACOMPRA BETWEEN :DTA1 AND :DTA2) AND (movdet.CODPRODUTO BETWEEN :PROD1 AND :PROD2)  and ((prod.TIPO <> 'SERV') or (prod.tipo is null)) AND ((mov.CODNATUREZA = :NATUREZA) OR (:NATUREZA = 100)) AND
-            ((mov.CODALMOXARIFADO = :CCUSTO) OR (:CCUSTO = 1))  and ((movdet.LOTE = :LOTE) or (:LOTE = 'TODOS OS LOTES CADASTRADOS NO SISTEMA'))
-            AND ((prod.CATEGORIA = :SUBGRUPO) OR (:SUBGRUPO = 'TODOS SUBGRUPOS DO CADASTRO CATEGORIA'))  
-            AND ((prod.MARCA = :MARCA) OR (:MARCA = 'TODAS AS MARCAS CADASTRADAS NO SISTEMA')) and movdet.BAIXA is not null 
-            AND ((prod.FAMILIA = :GRUPOPROC) OR (:GRUPOPROC = 'TODOS OS GRUPOS CADASTRADOS NO SISTEMA'))
-
-            group by  prod.FAMILIA, prod.CATEGORIA, prod.CODPRO, prod.PRODUTO, movdet.CODPRODUTO, l.codlote, movdet.LOTE
+        WHERE (v.DATACOMPRA BETWEEN :DTA1 AND :DTA2) 
+          AND (movdet.CODPRODUTO BETWEEN :PROD1 AND :PROD2)  
+          AND ((prod.TIPO <> 'SERV') or (prod.tipo is null)) 
+          AND ((mov.CODNATUREZA = :NATUREZA) OR (:NATUREZA = 100)) 
+          AND ((mov.CODALMOXARIFADO = :CCUSTO) OR (:CCUSTO = 1))  
+          AND ((movdet.LOTE = :LOTE) or (:LOTE = 'TODOS OS LOTES CADASTRADOS NO SISTEMA'))
+          AND ((prod.CATEGORIA = :SUBGRUPO) OR (:SUBGRUPO = 'TODOS SUBGRUPOS DO CADASTRO CATEGORIA'))  
+          AND ((prod.MARCA = :MARCA) OR (:MARCA = 'TODAS AS MARCAS CADASTRADAS NO SISTEMA')) 
+          AND movdet.BAIXA is not null 
+          AND natu.BAIXAMOVIMENTO = 0
+          AND ((prod.FAMILIA = :GRUPOPROC) OR (:GRUPOPROC = 'TODOS OS GRUPOS CADASTRADOS NO SISTEMA'))
+          group by  prod.FAMILIA, prod.CATEGORIA, prod.CODPRO, prod.PRODUTO, movdet.CODPRODUTO, l.codlote, movdet.LOTE
                 , mov.datamovimento, mov.codNatureza
                 , natu.BAIXAMOVIMENTO, natu.DESCNATUREZA
                 , mov.CODMOVIMENTO, movdet.DTAFAB, 
