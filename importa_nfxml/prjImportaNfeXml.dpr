4d457439949c1a2cc9cb3a5e4b4b3892e8d5ec53
crsilveira@gmail.com <crsilveira@gmail.com@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2013-06-25 12:47:46 +0000

    desenv.

--- importa_nfxml/prjImportaNfeXml.dpr
+++ importa_nfxml/prjImportaNfeXml.dpr
@@ -2,12 +2,16 @@ program prjImportaNfeXml;
 
 uses
   Forms,
-  uimporta_nfxml in 'uimporta_nfxml.pas' {Form1};
+  uimporta_nfxml in 'uimporta_nfxml.pas' {fImporta_XML},
+  uProdutoFornecedor in 'uProdutoFornecedor.pas' {fProdutoFornec},
+  uProdutoProc in 'uProdutoProc.pas' {fProdutoProc};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TfImporta_XML, fImporta_XML);
+  Application.CreateForm(TfProdutoFornec, fProdutoFornec);
+  Application.CreateForm(TfProdutoProc, fProdutoProc);
   Application.Run;
 end.
