e9a4d5307fbbd441de9b1f60a0c0d3e630714d85
crsilveira@gmail.com <crsilveira@gmail.com@223bc6e6-d04e-11de-a697-fbeabf64b1f8>
2013-07-22 15:08:39 +0000

    Alterado a rotina de atualizar estoque, para não fazer várias vezes.

--- trunk/comercial/src/uEstoqueAtualiza.pas
+++ trunk/comercial/src/uEstoqueAtualiza.pas
@@ -22,75 +22,113 @@ procedure TEstoqueAtualiza.Execute;
 var strBuscaItem: string;
   strEstoqueAtual: String;
   strAtualiza: String;
+  strAtualizaLote: String;
   sqlBuscaEstoque: TSqlQuery;
+  sqlB: TSqlQuery;
+  strMudaStatus: String;
   TDA: TTransactionDesc;
 begin
-  TDA.TransactionID  := 1;
-  TDA.IsolationLevel := xilREADCOMMITTED;
-
-  sqlBuscaEstoque := TSqlQuery.Create(nil);
+  sqlB := TSqlQuery.Create(nil);
   try
-    sqlBuscaEstoque.SQLConnection := dm.sqlsisAdimin;
+    sqlB.SQLConnection := dm.sqlsisAdimin;
+    sqlB.sql.Add('SELECT INSERIDO, DATA_MODIFICADO FROM ATUALIZA WHERE CODATUALIZA = 5000');
+    sqlB.Open;
+    strMudaStatus := 'S';
+    if ((sqlB.FieldByName('INSERIDO').AsString = 'N') or (sqlB.FieldByName('INSERIDO').IsNull)) then
+      strMudaStatus := 'N';
+  finally
+    sqlB.Destroy;
+  end;
+
+  if (strMudaStatus = 'N') then
+  begin
+    TDA.TransactionID  := 1;
+    TDA.IsolationLevel := xilREADCOMMITTED;
+    dm.sqlsisAdimin.StartTransaction(TDA);
+    try
+      dm.sqlsisAdimin.ExecuteDirect('UPDATE ATUALIZA SET INSERIDO = ' +
+        QuotedStr('S') +
+        ' , DATA_MODIFICADO = ' + QuotedStr(formatdatetime('mm/dd/yy', now)) +
+        ' WHERE CODATUALIZA = 5000');
+      dm.sqlsisAdimin.Commit(TDA);
+      strMudaStatus := 'S';
+    except
+      on E : Exception do
+      begin
+        dm.sqlsisAdimin.Rollback(TDA); //on failure, undo the changes}
+      end;
+    end;
+
     Priority := tpLower;
     if (dm.cdsBusca.Active) then
       dm.cdsBusca.Close;
-    dm.cdsBusca.CommandText := 'SELECT FIRST 50 DISTINCT MD.CODPRODUTO, M.CODALMOXARIFADO, MD.LOTE ' +
-      '  FROM MOVIMENTO M, MOVIMENTODETALHE MD, NATUREZAOPERACAO NP  ' +
-      ' WHERE M.CODMOVIMENTO = MD.CODMOVIMENTO ' +
-      '   AND M.CODNATUREZA = NP.CODNATUREZA ' +
-      '   AND NP.BAIXAMOVIMENTO in (0,1)' +
-      '   ORDER BY M.CODMOVIMENTO DESC ';
+    dm.cdsBusca.CommandText := 'SELECT FIRST 50 DISTINCT MD.CODPRODUTO, ' +
+     ' M.CODALMOXARIFADO, MD.LOTE , ' +
+     ' ve.PRECO_CUSTO, ve.ESTOQUE, ve.PRECO_COMPRA ' +
+     '  FROM MOVIMENTO M, MOVIMENTODETALHE MD, NATUREZAOPERACAO NP, VIEW_ESTOQUE ve ' +
+     ' WHERE M.CODMOVIMENTO = MD.CODMOVIMENTO ' +
+     '   AND M.CODNATUREZA = NP.CODNATUREZA ' +
+     '   AND MD.CODPRODUTO = ve.CODPRODUTO ' +
+     '   AND NP.BAIXAMOVIMENTO in (0,1) ' +
+     '   ORDER BY M.CODMOVIMENTO DESC ';
     dm.cdsBusca.Open;
-    while not dm.cdsBusca.eof do
-    begin
-      if (sqlBuscaEstoque.active) then
-        sqlBuscaEstoque.Close;
-      sqlBuscaEstoque.sql.clear;
-      strEstoqueAtual := 'select ev.PRECOCUSTO, ev.SALDOFIMACUM, ev.PRECOCOMPRA' +
-        '   from ESTOQUE_VIEW_CUSTO( ' +
-        '       current_date,' +
-        IntToStr(dm.cdsBusca.FieldByName('CODPRODUTO').asInteger) + ', ' +
-        IntToStr(dm.cdsBusca.FieldByName('CODALMOXARIFADO').asInteger) + ', ';
-        if ((dm.cdsBusca.FieldByName('LOTE').isnull) or (dm.cdsBusca.FieldByName('LOTE').asString = '0')) then
-          strEstoqueAtual := strEstoqueAtual + QuotedStr('TODOS OS LOTES CADASTRADOS NO SISTEMA')
-        else
-          strEstoqueAtual := strEstoqueAtual + QuotedStr(dm.cdsBusca.FieldByName('LOTE').asString);
-        strEstoqueAtual := strEstoqueAtual + ') ev  ';
-      sqlBuscaEstoque.sql.Add(strEstoqueAtual);
-      sqlBuscaEstoque.Open;
-      if (not sqlBuscaEstoque.IsEmpty) then      // Nao achou nada no sistema
+    DecimalSeparator := '.';
+    dm.sqlsisAdimin.StartTransaction(TDA);
+    try
+      while not dm.cdsBusca.eof do
       begin
         strAtualiza := 'UPDATE PRODUTOS SET VALORUNITARIOATUAL = ';
-        try
-          DecimalSeparator := '.';
-          strAtualiza := strAtualiza + FloatToStr(sqlBuscaEstoque.FieldByName('PRECOCOMPRA').asfloat);
-          strAtualiza := strAtualiza + ' , PRECOMEDIO = ';
-          strAtualiza := strAtualiza + FloatToStr(sqlBuscaEstoque.FieldByName('PRECOCUSTO').asfloat);
-          strAtualiza := strAtualiza + ' , ESTOQUEATUAL = ';
-          strAtualiza := strAtualiza + FloatToStr(sqlBuscaEstoque.FieldByName('SALDOFIMACUM').asfloat);
-          strAtualiza := strAtualiza + ' WHERE CODPRODUTO = ' +
-            IntToStr(dm.cdsBusca.FieldByName('CODPRODUTO').asInteger);
-          dm.sqlsisAdimin.StartTransaction(TDA);
-          try
-            dm.sqlsisAdimin.ExecuteDirect(strAtualiza);
-            dm.sqlsisAdimin.Commit(TDA);
-          except
-            on E : Exception do
-            begin
-              dm.sqlsisAdimin.Rollback(TDA); //on failure, undo the changes}
-            end;
+        strAtualiza := strAtualiza + FloatToStr(dm.cdsBusca.FieldByName('PRECO_COMPRA').asfloat);
+        strAtualiza := strAtualiza + ' , PRECOMEDIO = ';
+        strAtualiza := strAtualiza + FloatToStr(dm.cdsBusca.FieldByName('PRECO_CUSTO').asfloat);
+        strAtualiza := strAtualiza + ' , ESTOQUEATUAL = ';
+        strAtualiza := strAtualiza + FloatToStr(dm.cdsBusca.FieldByName('ESTOQUE').asfloat);
+        strAtualiza := strAtualiza + ' WHERE CODPRODUTO = ' +
+        IntToStr(dm.cdsBusca.FieldByName('CODPRODUTO').asInteger);
+        dm.sqlsisAdimin.ExecuteDirect(strAtualiza);
+        // atualiza lote
+        {if (dm.cdsBusca.FieldByName('LOTE').asString <> '0') then
+        begin
+          if (sqlBuscaEstoque.active) then
+            sqlBuscaEstoque.Close;
+          sqlBuscaEstoque.sql.clear;
+          strEstoqueAtual := 'SELECT ESTOQUE, PRECO FROM LOTES WHERE CODPRODUTO = ' +
+            IntToStr(dm.cdsBusca.FieldByName('CODPRODUTO').asInteger) +
+            ' AND LOTE = ' + QuotedStr(dm.cdsBusca.FieldByName('LOTE').asString);
+          sqlBuscaEstoque.SQL.Add(strEstoqueAtual);
+          sqlBuscaEstoque.Open;
+          if (sqlBuscaEstoque.IsEmpty) then
+          begin
+            strAtualizaLote := 'INSERT INTO LOTE, CODPRODUTO, DATAFABRICACAO ' +
+              ', DATAVENCIMENTO, ESTOQUE, PRECO, NOTAFISCAL, SERIEINI, SERIEFIM '
+          end
+          else
+          begin
           end;
-        finally
-          DecimalSeparator := ',';
-        end;
-      end;  
-      dm.cdsBusca.next;
+        end;}
+        dm.cdsBusca.next;
+      end;
+      DecimalSeparator := ',';
+      dm.sqlsisAdimin.Commit(TDA);
+    except
+      on E : Exception do
+      begin
+        dm.sqlsisAdimin.Rollback(TDA); //on failure, undo the changes}
+      end;
     end;
-  finally
-    sqlBuscaEstoque.Destroy;
+    dm.sqlsisAdimin.StartTransaction(TDA);
+    try
+      dm.sqlsisAdimin.ExecuteDirect('UPDATE ATUALIZA SET INSERIDO = ' + QuotedStr('N') +
+          ' , DATA_MODIFICADO = null ' +
+          ' WHERE CODATUALIZA = 5000');
+      dm.sqlsisAdimin.Commit(TDA);
+    except
+      on E : Exception do
+      begin
+        dm.sqlsisAdimin.Rollback(TDA); //on failure, undo the changes}
+      end;
+    end;
   end;
-  inherited;
-
 end;
 
 end.
